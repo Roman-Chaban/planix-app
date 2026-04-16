edac68be1e0cf1fce63d537a365064a50d1eb06b
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-16 23:31:31 +0300

    Feat: Developed reusable Button component with its variants, sizes, outlines, added types and index for clarity and better consistency.

---
+++ src/shared/ui/button/types.ts
@@ -0,0 +1,22 @@
+import type { HTMLAttributes, ReactNode } from "react";
+
+export type ButtonVariant = "primary" | "outline" | "soft" | "tertiary";
+export type ButtonSize = "sm" | "md" | "lg";
+export type ButtonOutline =
+  | "white"
+  | "light-blue"
+  | "transparent"
+  | "disabled"
+  | "double-outline"
+  | "none";
+
+export type ButtonProps = {
+  children: ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  outline?: ButtonOutline;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+} & HTMLAttributes<HTMLButtonElement>;

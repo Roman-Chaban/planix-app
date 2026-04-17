e6bfbce3bf8e707533aa0fe63cc45331381fae0c
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-17 19:22:25 +0300

    Feat: Added reset styles into layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import { DM_Sans } from "next/font/google";
 
+import "@shared/styles/reset/_reset.scss";
+
 const dm_sans = DM_Sans({
   weight: ["400", "500", "600", "700", "800"],
   subsets: ["latin"],

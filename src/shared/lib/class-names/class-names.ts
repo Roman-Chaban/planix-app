5f2e278568cd474f1bcaff50315e5b7c7c0070f9
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-17 18:17:38 +0300

    Feat: Redid classNames helper function for better generation and added new types.

--- src/shared/lib/class-names/class-names.ts
+++ src/shared/lib/class-names/class-names.ts
@@ -1,5 +1,34 @@
-import type { ClassValue } from "./types";
+import type { ClassObject, ClassValue } from "./types";
+
+const isString = (value: ClassValue): value is string => {
+  return typeof value === "string";
+};
+
+const isObject = (value: ClassValue): value is ClassObject => {
+  return typeof value === "object" && value !== null;
+};
 
 export const classNames = (...values: ClassValue[]): string => {
-  return values.filter(Boolean).join(" ");
+  const result: string[] = [];
+
+  for (const value of values) {
+    if (!value) continue;
+
+    if (isString(value)) {
+      result.push(value);
+      continue;
+    }
+
+    if (isObject(value)) {
+      for (const key in value) {
+        if (Object.prototype.hasOwnProperty.call(value, key)) {
+          if (value[key]) {
+            result.push(key);
+          }
+        }
+      }
+    }
+  }
+
+  return result.join(" ");
 };

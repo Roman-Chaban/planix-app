d8d3f1300e348979df9b9c99c314370fc6e343b7
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-20 23:17:52 +0300

    Refactor: Added id attribute into Typography component.

--- src/shared/ui/typography/Typography.tsx
+++ src/shared/ui/typography/Typography.tsx
@@ -1,12 +1,18 @@
 import type { FC } from "react";
+
 import type { TypographyProps } from "./types";
 
 export const Typography: FC<TypographyProps> = ({
   as = "span",
   children,
-  className,
+  id = "",
+  className = "",
 }) => {
   const Component = as;
 
-  return <Component className={className}>{children}</Component>;
+  return (
+    <Component id={id} className={className}>
+      {children}
+    </Component>
+  );
 };

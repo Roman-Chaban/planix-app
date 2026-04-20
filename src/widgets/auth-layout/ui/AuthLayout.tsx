47ee184bec29e312c9a9c0b6d34891498c9df68f
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-20 23:16:32 +0300

    Refactor: Separeted JSX logic to make clean component, updated styles.

--- src/widgets/auth-layout/ui/AuthLayout.tsx
+++ src/widgets/auth-layout/ui/AuthLayout.tsx
@@ -1,33 +1,23 @@
 import type { FC } from "react";
+
+import { LoginForm } from "@/features/auth/auth-login/ui/index";
+import { Box } from "@/shared/ui/box/index";
+import { BrandBanner } from "@/shared/ui/brand-banner/index";
 import { Grid, GridItem } from "@/shared/ui/grid/index";
 
-import { Avatar } from "@/shared/ui/avatar";
-import { PlanixLogo } from "@/shared/ui/icons/Icons";
-import { Box } from "@/shared/ui/box/index";
-import { Typography } from "@/shared/ui/typography/Typography";
 
 import styles from "./auth-layout.module.scss";
 
 export const AuthLayout: FC = () => {
   return (
     <Box className={styles.wrapper}>
-      <Grid>
-        <GridItem span={6}>
-          <Box className={styles.banner}>
-            <Box className={styles.logoContainer}>
-              <Avatar
-                alt="Palnix Logo"
-                icon={<PlanixLogo width={78} height={78} />}
-              />
+      <Grid className={styles.wrapperContainer}>
+        <GridItem span={7}>
+          <BrandBanner />
+        </GridItem>
 
-              <Typography as="span" className={styles.logoCapture}>
-                Planix
-              </Typography>
-            </Box>
-            <Typography as="h5" className={styles.logoHeading}>
-              Empowering You to Plan, Track, and Deliver with Clarity
-            </Typography>
-          </Box>
+        <GridItem span={5}>
+          <LoginForm />
         </GridItem>
       </Grid>
     </Box>

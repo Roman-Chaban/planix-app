0c2dfd8545d47f9f443e775d843ed4f15fb60cad
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-20 23:12:08 +0300

    Feat: Created Avatar component for images, icons, added fallback text.

--- src/shared/ui/avatar/Avatar.tsx
+++ src/shared/ui/avatar/Avatar.tsx
@@ -1,10 +1,13 @@
 import type { FC } from "react";
+
 import Image from "next/image";
 
 import { Box } from "@shared/ui/box/Box";
-import type { AvatarProps } from "./types";
 
 import styles from "./avatar.module.scss";
+
+import type { AvatarProps } from "./types";
+
 
 export const Avatar: FC<AvatarProps> = ({
   src,
@@ -31,7 +34,7 @@ export const Avatar: FC<AvatarProps> = ({
       ) : src ? (
         <Image src={src} alt={alt} fill className={styles.image} />
       ) : (
-        <Box className={styles.fallback}>{fallback ?? "?"}</Box>
+        <Box className={styles.fallback}>{fallback ?? "Fallback Icon"}</Box>
       )}
     </Box>
   );

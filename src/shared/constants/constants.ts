3b0530378f8032443dc1423ad251a25b9e99449a
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-20 23:09:38 +0300

    Feat: Created constants for better clarity and consistency.

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+};
+
+export const LinkTargets = {
+  BLANK: "_blank",
+  SELF: "_self",
+  PARENT: "_parent",
+  TOP: "_top",
+  FRAMENAME: "framename",
+};

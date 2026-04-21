461ca1db067a458d5ce40ec650eb88b934ca67c4
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-21 21:26:57 +0300

    Feat: Added new INPUT_TYPES & BUTTON_TYPES constants, fixed other constants for clarity.

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,12 +1,26 @@
 export const ROUTES = {
   HOME: "/",
   LOGIN: "/login",
-};
+} as const;
 
-export const LinkTargets = {
+export const LINK_TARGETS = {
   BLANK: "_blank",
   SELF: "_self",
   PARENT: "_parent",
   TOP: "_top",
   FRAMENAME: "framename",
-};
+} as const;
+
+export const INPUT_TYPES = {
+  TEXT: "text",
+  CHECKBOX: "checkbox",
+  TEL: "tel",
+  EMAIL: "email",
+  PASSWORD: "password",
+} as const;
+
+export const BUTTON_TYPES = {
+  BUTTON: "button",
+  RESET: "reset",
+  SUBMIT: "submit",
+} as const;

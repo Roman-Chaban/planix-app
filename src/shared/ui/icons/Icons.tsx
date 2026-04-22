a7679cb8971004ae18e0d1a5ffe442f01a69f7cb
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-22 22:33:10 +0300

    Feat: Added HiddenIcon, VisibleIcon, SearchIcon, updated icons exports.

--- src/shared/ui/icons/Icons.tsx
+++ src/shared/ui/icons/Icons.tsx
@@ -147,11 +147,11 @@ export const LockIcon: FC<IconProps> = ({ width = 20, height = 20 }) => {
   );
 };
 
-export const VisibleIcon: FC<IconProps> = () => {
+export const HiddenIcon: FC<IconProps> = ({ width = 20, height = 20 }) => {
   return (
     <svg
-      width="20"
-      height="20"
+      width={width}
+      height={height}
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -193,6 +193,60 @@ export const VisibleIcon: FC<IconProps> = () => {
       />
       <path
         d="M18.3333 1.66675L12.1083 7.89175"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const VisibleIcon: FC<IconProps> = ({ width = 20, height = 20 }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.5799 11.9999C15.5799 13.9799 13.9799 15.5799 11.9999 15.5799C10.0199 15.5799 8.41992 13.9799 8.41992 11.9999C8.41992 10.0199 10.0199 8.41992 11.9999 8.41992C13.9799 8.41992 15.5799 10.0199 15.5799 11.9999Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.9998 20.27C15.5298 20.27 18.8198 18.19 21.1098 14.59C22.0098 13.18 22.0098 10.81 21.1098 9.39997C18.8198 5.79997 15.5298 3.71997 11.9998 3.71997C8.46984 3.71997 5.17984 5.79997 2.88984 9.39997C1.98984 10.81 1.98984 13.18 2.88984 14.59C5.17984 18.19 8.46984 20.27 11.9998 20.27Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const SearchIcon: FC<IconProps> = ({ width = 20, height = 20 }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11 20C15.9706 20 20 15.9706 20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z"
         stroke="currentColor"
         strokeWidth="1.5"
         strokeLinecap="round"
--- src/shared/ui/icons/index.ts
+++ src/shared/ui/icons/index.ts
@@ -7,4 +7,6 @@ export type { IconProps } from "@/shared/ui/icons/model/icons.types";
 export { ProfileIcon } from "@/shared/ui/icons/Icons";
 export { PostIcon } from "@/shared/ui/icons/Icons";
 export { LockIcon } from "@/shared/ui/icons/Icons";
+export { HiddenIcon } from "@/shared/ui/icons/Icons";
 export { VisibleIcon } from "@/shared/ui/icons/Icons";
+export { SearchIcon } from "@/shared/ui/icons/Icons";

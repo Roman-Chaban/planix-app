b1a7f190c18bd1ce510a0c1c998ddd1200e94122
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-27 19:33:06 +0300

    Feat: Configured i18n config.

---
+++ src/shared/config/i18n/i18nConfig.ts
@@ -0,0 +1,31 @@
+import i18n, { InitOptions } from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "@/shared/locales/en/en.json";
+import uk from "@/shared/locales/uk/uk.json";
+
+const resources = {
+  en: { translation: en },
+  uk: { translation: uk },
+};
+
+const options: InitOptions = {
+  resources,
+
+  fallbackLng: "en",
+  supportedLanguages: ["en", "uk"],
+
+  defaultNS: "translation",
+
+  interpolation: {
+    escapeValue: false,
+  },
+
+  react: {
+    useSuspense: false,
+  },
+};
+
+i18n.use(initReactI18next).init(options);
+
+export default i18n;

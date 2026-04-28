e40c682c5066b66c29b9189756195e1219d27a87
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-28 20:21:07 +0300

    Refactor: Change $shadow-elevation-1 property.

--- src/shared/ui/link/AppLink.tsx
+++ src/shared/ui/link/AppLink.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 
 import Link from 'next/link';
 
-import { LINK_TARGETS } from '@/shared/constants/constants';
+import { LINK_TARGETS } from '@/shared/constants/common';
 import { classNames } from '@/shared/lib/helpers/class-names';
 
 import styles from './styles.module.scss';

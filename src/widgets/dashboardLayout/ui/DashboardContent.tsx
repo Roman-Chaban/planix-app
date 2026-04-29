2089677dea907511c13fe7b558b7068fa9d52b19
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-30 00:29:59 +0300

    Feat: Add DashboardCards component.

--- src/widgets/dashboardLayout/ui/DashboardContent.tsx
+++ src/widgets/dashboardLayout/ui/DashboardContent.tsx
@@ -1,9 +1,14 @@
 import type { FC } from 'react';
 
+import { DashboardCards } from '@/widgets/dashboardLayout/ui/DashboardCards';
 import { Box } from '@/shared/ui';
 
 import styles from './styles.module.scss';
 
 export const DashboardContent: FC = () => {
-  return <Box className={styles.content}>Content</Box>;
+  return (
+    <Box className={styles.content}>
+      <DashboardCards />
+    </Box>
+  );
 };

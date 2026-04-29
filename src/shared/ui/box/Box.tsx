03f7b696160103c80f9fca2670109303d22b75ee
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-30 00:22:19 +0300

    Feat: Add boxProps, and add ComponentPropsWithoutRef type.

--- src/shared/ui/box/Box.tsx
+++ src/shared/ui/box/Box.tsx
@@ -2,8 +2,17 @@ import type { BoxProps } from './model/types';
 
 import type { ElementType } from 'react';
 
-export const Box = <T extends ElementType = 'div'>({ as, children, className }: BoxProps<T>) => {
+export const Box = <T extends ElementType = 'div'>({
+  as,
+  children,
+  className,
+  ...boxProps
+}: BoxProps<T>) => {
   const Component = as || 'div';
 
-  return <Component className={className}>{children}</Component>;
+  return (
+    <Component className={className} {...boxProps}>
+      {children}
+    </Component>
+  );
 };

4851ac9c564f0de27a2b193acaa2044a0dbee867
Roman Chaban <roman.chaban1001@gmail.com>
2026-04-30 20:51:35 +0300

    Refactor: Fix import name.

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { AuthLayout } from '@/widgets/authLayout/ui/AuthLayout';
+import { AuthLayout } from '@/widgets/auth/ui/AuthLayout';
 
 export default function LoginPage() {
   return <AuthLayout />;

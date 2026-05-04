a24033ebcae29799fca446e5f39b18c1bf900e28
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-04 23:59:21 +0300

    Feat: Add MEDIA constants.

--- src/shared/constants/breakpoints.ts
+++ src/shared/constants/breakpoints.ts
@@ -6,3 +6,12 @@ export const BREAKPOINTS = {
   DESKTOP: '1280',
   WIDE: '1536',
 } as const;
+
+export const MEDIA = {
+  MOBILE: `(max-width: ${BREAKPOINTS.MOBILE}px)`,
+  MOBILE_LARGE: `max-width: ${BREAKPOINTS.MOBILE_LARGE}px`,
+  TABLET: `(max-width: ${BREAKPOINTS.TABLET}px)`,
+  LAPTOP: `(max-width: ${BREAKPOINTS.LAPTOP}px)`,
+  DESKTOP: `(max-width: ${BREAKPOINTS.DESKTOP}px)`,
+  WIDE: `max-width: ${BREAKPOINTS.WIDE}px`,
+};

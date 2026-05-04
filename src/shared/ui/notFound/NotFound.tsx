3ba637c7bfbb975c9dc24e55c0b7a26c2f769605
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-04 19:24:16 +0300

    Refactor: Mock useTranslation and translation keys, add static content.

--- src/shared/ui/notFound/NotFound.tsx
+++ src/shared/ui/notFound/NotFound.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import { type FC } from 'react';
+
+// import { useTranslation } from 'react-i18next';
 
 import { NotFoundBanner } from '@/shared/ui/icons/index';
 import { Box, Grid, GridItem, Typography } from '@/shared/ui/index';
 
 import styles from './styles.module.scss';
-import '@/shared/styles/reset/_reset.scss';
 
 export const NotFoundLayout: FC = () => {
+  // const { t } = useTranslation();
+
   return (
     <Box className={styles.layout}>
       <Grid className={styles.container}>
@@ -16,12 +21,15 @@ export const NotFoundLayout: FC = () => {
         <GridItem span={6}>
           <Box className={styles.errorBlock}>
             <Typography as="h1" className={styles.errorTitle}>
+              {/* {t('NotFound.error')} */}
               Error
             </Typography>
             <Typography as="p" className={styles.errorSubtitle}>
+              {/* {t('NotFound.404')} */}
               404
             </Typography>
             <Typography as="h2" className={styles.errorText}>
+              {/* {t('NotFound.pageNotFound')} */}
               Page Not Found
             </Typography>
           </Box>

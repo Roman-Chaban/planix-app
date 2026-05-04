9078ce838a461edebdf2913958baba492c2ca838
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-04 19:25:58 +0300

    Refactor: Make better DASHBOARD_ROUTE, rename to LocaleLayout, delete html base tags from LocaleLayout, move them to RootLayout.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,24 +1,13 @@
 import type { ReactNode } from 'react';
 
-import { DM_Sans } from 'next/font/google';
-import { notFound } from 'next/navigation';
-
-import '@shared/styles/reset/_reset.scss';
-
 import { LOCALES, type Locale } from '@/shared/config/i18n/lib/locales';
-import { isLocale } from '@/shared/lib/helpers/isLocale/isLocale';
 import { GlobalProviders } from '@/shared/providers/GlobalProviders';
 
-const dm_sans = DM_Sans({
-  weight: ['400', '500', '600', '700', '800'],
-  subsets: ['latin'],
-});
-
-export function generateStaticParams(): { locale: Locale }[] {
+export function generateStaticParams() {
   return LOCALES.map((locale) => ({ locale }));
 }
 
-export default async function RootLayout({
+export default async function LocaleLayout({
   children,
   params,
 }: {
@@ -27,13 +16,5 @@ export default async function RootLayout({
 }) {
   const { locale } = await params;
 
-  if (!isLocale(locale)) notFound();
-
-  return (
-    <html lang={locale}>
-      <body className={dm_sans.className}>
-        <GlobalProviders locale={locale}>{children}</GlobalProviders>
-      </body>
-    </html>
-  );
+  return <GlobalProviders locale={locale as Locale}>{children}</GlobalProviders>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from 'next/navigation';
 
-import { LOCALES, ROUTES } from '@/shared/constants/common';
+import { DEFAULT_LOCALE } from '@/shared/config/i18n/lib/locales';
+import { ROUTES } from '@/shared/constants/common';
 
-const { EN } = LOCALES;
 const { DASHBOARD } = ROUTES;
 
-const DASHBOARD_ROUTE = `/${EN}/${DASHBOARD}`;
+const DASHBOARD_ROUTE = `/${DEFAULT_LOCALE}/${DASHBOARD}`;
 
 export default function HomePage() {
   redirect(DASHBOARD_ROUTE);

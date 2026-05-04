d359ca168ba1470f05d01731947b1266f6159f4b
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-04 20:14:06 +0300

    Refactor: Replace transaltion text with the help of NotFoundTextDecartion icon, delete useless style rules for content.

--- src/shared/ui/notFound/NotFound.tsx
+++ src/shared/ui/notFound/NotFound.tsx
@@ -2,16 +2,12 @@
 
 import { type FC } from 'react';
 
-// import { useTranslation } from 'react-i18next';
-
-import { NotFoundBanner } from '@/shared/ui/icons/index';
-import { Box, Grid, GridItem, Typography } from '@/shared/ui/index';
+import { NotFoundBanner, NotFoundTextDecartionIcon } from '@/shared/ui/icons/index';
+import { Box, Grid, GridItem } from '@/shared/ui/index';
 
 import styles from './styles.module.scss';
 
 export const NotFoundLayout: FC = () => {
-  // const { t } = useTranslation();
-
   return (
     <Box className={styles.layout}>
       <Grid className={styles.container}>
@@ -20,18 +16,7 @@ export const NotFoundLayout: FC = () => {
         </GridItem>
         <GridItem span={6}>
           <Box className={styles.errorBlock}>
-            <Typography as="h1" className={styles.errorTitle}>
-              {/* {t('NotFound.error')} */}
-              Error
-            </Typography>
-            <Typography as="p" className={styles.errorSubtitle}>
-              {/* {t('NotFound.404')} */}
-              404
-            </Typography>
-            <Typography as="h2" className={styles.errorText}>
-              {/* {t('NotFound.pageNotFound')} */}
-              Page Not Found
-            </Typography>
+            <NotFoundTextDecartionIcon />
           </Box>
         </GridItem>
       </Grid>

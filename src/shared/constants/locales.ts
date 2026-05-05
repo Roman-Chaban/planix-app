2abaec24b58280f72b5b53e0fac50e7a3ec7a988
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-05 14:54:20 +0300

    Refactor: Rename APP_ROUTES to better APP_LOCALES.

--- src/shared/constants/locales.ts
+++ src/shared/constants/locales.ts
@@ -1,9 +1,9 @@
-export const APP_ROUTES = {
+export const APP_LOCALES = {
   EN: 'en',
   UK: 'uk',
 } as const;
 
-export const LOCALE_VALUES = Object.values(APP_ROUTES);
+export const LOCALE_VALUES = Object.values(APP_LOCALES);
 
 export const LOCALES = ['en', 'uk'] as const;
 

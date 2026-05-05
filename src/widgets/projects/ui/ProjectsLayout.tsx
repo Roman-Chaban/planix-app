9db50a9f4f47a45dcc760a7c4f22a2b31f27a4de
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-05 19:27:06 +0300

    Feat: Add ProjectsLayout component.

---
+++ src/widgets/projects/ui/ProjectsLayout.tsx
@@ -0,0 +1,5 @@
+import { Header, PageWrapper } from '@/shared/ui';
+
+export const ProjectsLayout = () => {
+  return <PageWrapper header={<Header title={'DashboardHeaders.projects'} />}>Main</PageWrapper>;
+};

9c40fe27ac9ff3def739f1a1ae24522ce5ac4517
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-05 15:16:26 +0300

    Refactor: Rename BANNER_RESPONSIVE to ILLUSTRATION_SIZES_PROPS, delete dubplicate logic with isLaptopScreen.

--- src/shared/ui/notFound/ui/NotFound.tsx
+++ src/shared/ui/notFound/ui/NotFound.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { type FC } from 'react';
-
 import { BREAKPOINTS } from '@/shared/constants/breakpoints';
 import { useMediaQuery } from '@/shared/hooks/useMediaQuery';
 import { NotFoundBanner, NotFoundTextDecartionIcon } from '@/shared/ui/icons/index';
@@ -11,27 +9,19 @@ import styles from './styles.module.scss';
 
 const { LAPTOP } = BREAKPOINTS;
 
-export const NotFoundLayout: FC = () => {
-  const isLaptop = useMediaQuery(LAPTOP);
+export const NotFoundLayout = () => {
+  const isLaptopScreen = useMediaQuery(LAPTOP);
 
-  const BANNER_RESPONSIVE = isLaptop ? (
-    <NotFoundBanner width={330} height={330} />
-  ) : (
-    <NotFoundBanner />
-  );
-
-  const DECORATION_RESPONSIVE = isLaptop ? (
-    <NotFoundTextDecartionIcon width={330} height={330} />
-  ) : (
-    <NotFoundTextDecartionIcon />
-  );
+  const ILLUSTRATION_SIZES_PROPS = isLaptopScreen ? { width: 330, height: 330 } : {};
 
   return (
     <Box className={styles.layout}>
       <Grid className={styles.container}>
-        <GridItem span={6}>{BANNER_RESPONSIVE}</GridItem>
+        <GridItem span={6}>{<NotFoundBanner {...ILLUSTRATION_SIZES_PROPS} />}</GridItem>
         <GridItem span={6}>
-          <Box className={styles.errorBlock}>{DECORATION_RESPONSIVE}</Box>
+          <Box className={styles.errorBlock}>
+            {<NotFoundTextDecartionIcon {...ILLUSTRATION_SIZES_PROPS} />}
+          </Box>
         </GridItem>
       </Grid>
     </Box>

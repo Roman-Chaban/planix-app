b16e52ed5fd66ff56a708ff5773a9245276fbe5f
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-09 23:42:03 +0300

    Refactor: Cleanup ProjectsToolbar component.

--- src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
+++ src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
@@ -1,27 +1,7 @@
 import type { ProjectsTollbarProps } from '../model/types';
 
 import type { FC } from 'react';
-import { Controller } from 'react-hook-form';
 
-import { INPUT_VARIANTS } from '@/shared/constants/inputs';
-import { Input } from '@/shared/ui/input/lib';
-
-const { PRIMARY } = INPUT_VARIANTS;
-
-export const ProjectToolbar: FC<ProjectsTollbarProps> = ({ control }) => {
-  return (
-    <Controller
-      name="search"
-      control={control}
-      render={({ field }) => (
-        <Input.Search
-          variant={PRIMARY}
-          inputSize="md"
-          {...field}
-          placeholder="Search"
-          autoComplete="search"
-        />
-      )}
-    />
-  );
+export const ProjectToolbar: FC<ProjectsTollbarProps> = () => {
+  return <></>;
 };

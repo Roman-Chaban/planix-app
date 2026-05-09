b4d4d2ef85f8e574b0beafdfefd021dce93e1213
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-10 00:51:35 +0300

    Feat: Rename type names for clarity.

--- src/widgets/dashboard/ui/DashboardCards/model/types.ts
+++
@@ -1,4 +0,0 @@
-import type { DashboardCardProps } from '@/shared/ui/dashboardCard/model/types';
-
-export type DashboardCard = DashboardCardProps;
-export type DashboardCards = DashboardCard[];
--- src/widgets/dashboard/ui/dashboard-cards/model/types.ts
+++ src/widgets/dashboard/ui/dashboard-cards/model/types.ts
@@ -1,4 +1,4 @@
 import type { CardProps } from '@/shared/ui/dashboardCard/model/types';
 
-export type DashboardCard = CardProps;
-export type DashboardCards = DashboardCard[];
+export type Card = CardProps;
+export type Cards = Card[];

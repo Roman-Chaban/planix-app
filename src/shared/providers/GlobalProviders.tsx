0fcb0e964c39f04c4f75c8756f60d4905556cd73
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-09 23:48:39 +0300

    Feat: Add FormProvider into GlobalProviders.

--- src/shared/providers/GlobalProviders.tsx
+++ src/shared/providers/GlobalProviders.tsx
@@ -3,6 +3,12 @@ import type { FC } from 'react';
 import type { BasedProviderProps } from '@/shared/providers/model/types';
 import { TranslationProvider } from '@/shared/providers/TranslationProvider';
 
+import { FormProvider } from './FormProvider';
+
 export const GlobalProviders: FC<BasedProviderProps> = ({ children, locale }) => {
-  return <TranslationProvider locale={locale}>{children}</TranslationProvider>;
+  return (
+    <FormProvider>
+      <TranslationProvider locale={locale}>{children}</TranslationProvider>
+    </FormProvider>
+  );
 };

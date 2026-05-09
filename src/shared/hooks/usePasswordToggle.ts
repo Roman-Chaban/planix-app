14e2c14ca876af42f54fd98ea772e5f58319bb0c
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-09 23:48:05 +0300

    Feat: Move usePasswordToggle into hooks.

---
+++ src/shared/hooks/usePasswordToggle.ts
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const usePasswordToggle = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const toggle = () => setIsVisible((toggleable) => !toggleable);
+
+  return {
+    isVisible,
+    toggle,
+  };
+};

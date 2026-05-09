9dac5cf06d318949a9bfba5d9b27e5d56a9cc533
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-09 23:50:08 +0300

    Feat: Add FormProviderProps.

--- src/shared/providers/model/types.ts
+++ src/shared/providers/model/types.ts
@@ -1,8 +1,20 @@
+import type { ClassName } from '@types';
+
 import type { ReactNode } from 'react';
 
 import type { Locale } from '@/shared/constants/locales';
+import type { InputSize, InputVariant } from '@/shared/ui/input/model/types';
 
 export type BasedProviderProps = {
   children: ReactNode;
   locale: Locale;
+};
+
+export type FormProviderProps = {
+  children: ReactNode;
+  error?: string;
+  variant?: InputVariant;
+  inputSize?: InputSize;
+  disabled?: boolean;
+  className?: ClassName;
 };

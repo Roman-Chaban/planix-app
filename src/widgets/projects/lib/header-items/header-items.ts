3224dafa73c5c1e59acbcf8823a92d34094ee2b4
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-11 16:09:17 +0300

    Refactor: Rename folder and file for better FSD style.

--- src/widgets/projects/lib/header-items/header-items.ts
+++ src/widgets/projects/lib/header-items/header-items.ts
@@ -3,22 +3,22 @@ import type { HeaderItems } from '@/widgets/projects/ui/ProjectsHeader/model/typ
 export const headerItems: HeaderItems = [
   {
     id: 'allProjects',
-    label: 'PROJECTS_HEADER.allProjects',
+    label: 'ProjectsHeader.allProjects',
   },
   {
     id: 'inProgress',
-    label: 'PROJECTS_HEADER.inProgress',
+    label: 'ProjectsHeader.inProgress',
   },
   {
     id: 'pending',
-    label: 'PROJECTS_HEADER.pending',
+    label: 'ProjectsHeader.pending',
   },
   {
     id: 'completed',
-    label: 'PROJECTS_HEADER.completed',
+    label: 'ProjectsHeader.completed',
   },
   {
     id: 'canceled',
-    label: 'PROJECTS_HEADER.canceled',
+    label: 'ProjectsHeader.canceled',
   },
 ];

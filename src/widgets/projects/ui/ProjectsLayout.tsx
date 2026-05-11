0fcd644bb0a8cd13f10841a8ef03b88fea90626c
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-11 16:04:26 +0300

    Refactor: Correct styles imports, fix type name for clarity.

--- src/widgets/projects/ui/ProjectsLayout.tsx
+++ src/widgets/projects/ui/ProjectsLayout.tsx
@@ -1,11 +1,10 @@
 'use client';
 
+import styles from '@/widgets/projects/styles/projects.module.scss';
 import { ProjectsHeader } from '@/widgets/projects/ui/ProjectsHeader/ui/ProjectsHeader';
 import { ProjectToolbar } from '@/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar';
 import { useProjectsFilters } from '@/features/projects/hooks/useProjectsFilters';
 import { Box, Header, PageWrapper } from '@/shared/ui';
-
-import styles from '../styles/styles.module.scss';
 
 export const ProjectsLayout = () => {
   const { control } = useProjectsFilters();
--- src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
+++ src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 
-import type { ProjectsTollbarProps } from '@/widgets/projects/ui/ProjectsToolbar/model/types';
+import type { ProjectsToolbarProps } from '@/widgets/projects/ui/ProjectsToolbar/model/types';
 
-export const ProjectToolbar: FC<ProjectsTollbarProps> = () => {
+export const ProjectToolbar: FC<ProjectsToolbarProps> = () => {
   return <></>;
 };

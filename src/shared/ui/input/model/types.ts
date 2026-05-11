3c9ea99304a4b2c850bcc0ba4e470b286650359b
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-11 23:45:20 +0300

    Refactor: Change InputVariant types.

--- src/shared/ui/input/model/types.ts
+++ src/shared/ui/input/model/types.ts
@@ -1,5 +1,14 @@
 export type InputSize = 'sm' | 'md' | 'lg';
-export type InputVariant = 'primary' | 'typing' | 'filed' | 'error';
+
+export type InputVariant =
+  | 'default'
+  | 'search'
+  | 'hover'
+  | 'typing'
+  | 'filed'
+  | 'disabled'
+  | 'error';
+
 export type IconPosition = 'start' | 'end';
 export type InputTypes =
   | 'email'

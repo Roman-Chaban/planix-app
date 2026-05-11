c49ee45ea5c958338735178e3f9d78fcc371ffe5
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-11 20:08:01 +0300

    Refactor: Add FormProvider, delete useless Box ui, add search variant for input.

--- src/shared/ui/form-field/ui/FormField.tsx
+++ src/shared/ui/form-field/ui/FormField.tsx
@@ -1,10 +1,18 @@
 import { type FC } from 'react';
 
-import { buildClassName } from '@/shared/lib/helpers/buildClassName/buildClassName';
+import { FormProvider } from '@/shared/providers/FormProvider';
 import type { FormFieldProps } from '@/shared/ui/form-field/model/types';
-import styles from '@/shared/ui/form-field/styles/form-field.module.scss';
-import { Box } from '@/shared/ui/index';
 
-export const FormField: FC<FormFieldProps> = ({ children, className }) => {
-  return <Box className={buildClassName(styles.wrapper, className)}>{children}</Box>;
+export const FormField: FC<FormFieldProps> = ({
+  children,
+  variant = 'default',
+  inputSize = 'md',
+  error,
+  disabled,
+}) => {
+  return (
+    <FormProvider variant={variant} inputSize={inputSize} error={error} disabled={disabled}>
+      {children}
+    </FormProvider>
+  );
 };

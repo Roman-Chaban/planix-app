cf71d0608bb25f89ec4962113c2c825c5b032458
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-11 20:05:12 +0300

    Refactor: Delete FormProvider, move it to FormField.

--- src/shared/providers/GlobalProviders.tsx
+++ src/shared/providers/GlobalProviders.tsx
@@ -3,12 +3,6 @@ import type { FC } from 'react';
 import type { BasedProviderProps } from '@/shared/providers/model/types';
 import { TranslationProvider } from '@/shared/providers/TranslationProvider';
 
-import { FormProvider } from './FormProvider';
-
 export const GlobalProviders: FC<BasedProviderProps> = ({ children, locale }) => {
-  return (
-    <FormProvider>
-      <TranslationProvider locale={locale}>{children}</TranslationProvider>
-    </FormProvider>
-  );
+  return <TranslationProvider locale={locale}>{children}</TranslationProvider>;
 };

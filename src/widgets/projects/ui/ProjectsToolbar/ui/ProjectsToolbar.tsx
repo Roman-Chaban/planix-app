6f20b71e9cc25b3b1356ccd4e5b3dd555195243b
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 00:58:04 +0300

    Refactor: Include ProjectButton, fix toolbar styles.

--- src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
+++ src/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar.tsx
@@ -6,18 +6,15 @@ import { useTranslation } from 'react-i18next';
 
 import styles from '@/widgets/projects/styles/projects.module.scss';
 import type { ProjectsToolbarProps } from '@/widgets/projects/ui/ProjectsToolbar/model/types';
-import { BUTTON_TYPES, BUTTON_VARIANTS } from '@/shared/constants/buttons';
 import { ICON_POSITION } from '@/shared/constants/common';
 import { INPUT_TYPES } from '@/shared/constants/inputs';
-import { Box, Button } from '@/shared/ui';
+import { Box, ProjectButton } from '@/shared/ui';
 import { FormField, FormIcon, FormWrapper } from '@/shared/ui/form-field';
 import { SearchIconPrimary } from '@/shared/ui/icons';
 import { Input } from '@/shared/ui/input/ui/Input';
 
 const { START } = ICON_POSITION;
 const { SEARCH } = INPUT_TYPES;
-const { BUTTON } = BUTTON_TYPES;
-const { PRIMARY } = BUTTON_VARIANTS;
 
 export const ProjectToolbar: FC<ProjectsToolbarProps> = () => {
   const { t } = useTranslation();
@@ -33,9 +30,7 @@ export const ProjectToolbar: FC<ProjectsToolbarProps> = () => {
         </FormWrapper>
       </FormField>
 
-      <Button type={BUTTON} variant={PRIMARY} size="md" className={styles.button}>
-        {t('ProjectsHeader.button')}
-      </Button>
+      <ProjectButton />
     </Box>
   );
 };

be734d36b481e865ca32d4caffc6196d335830ba
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 00:52:29 +0300

    Feat: Add QueryProvider to GlobalProviders.

--- src/shared/providers/GlobalProviders.tsx
+++ src/shared/providers/GlobalProviders.tsx
@@ -1,8 +1,13 @@
 import type { FC } from 'react';
 
 import type { BasedProviderProps } from '@/shared/providers/model/types';
+import { QueryProvider } from '@/shared/providers/QueryProvider';
 import { TranslationProvider } from '@/shared/providers/TranslationProvider';
 
 export const GlobalProviders: FC<BasedProviderProps> = ({ children, locale }) => {
-  return <TranslationProvider locale={locale}>{children}</TranslationProvider>;
+  return (
+    <QueryProvider>
+      <TranslationProvider locale={locale}>{children}</TranslationProvider>
+    </QueryProvider>
+  );
 };

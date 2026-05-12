69c6fc8ab9492d50974884a906f22356fc9b60d6
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 00:53:50 +0300

    Feat: Add ProjectButton, StatusBadge exports.

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -13,3 +13,5 @@ export { LanguageSelect } from '@/shared/ui/language-select/ui/LanguageSelect';
 export { NotFoundLayout } from '@/shared/ui/not-found/ui/NotFound';
 export { Header } from '@shared/ui/header/ui/Header';
 export { PageWrapper } from '@shared/ui/page-wrapper/ui/PageWrapper';
+export { ProjectButton } from '@shared/ui/project-button/ui/ProjectButton';
+export { StatusBadge } from '@shared/ui/status-badge/ui/StatusBadge';

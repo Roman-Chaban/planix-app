6c8047e15386758fb0daf62e348716993f48f525
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-14 01:01:34 +0300

    Refactor: rename recourses and options for better readable.

--- src/shared/config/i18n/i18nConfig.ts
+++ src/shared/config/i18n/i18nConfig.ts
@@ -4,13 +4,13 @@ import { initReactI18next } from 'react-i18next';
 import en from '@/shared/locales/en.json';
 import uk from '@/shared/locales/uk.json';
 
-const resources = {
+const localesResources = {
   en: { translation: en },
   uk: { translation: uk },
 };
 
-const options = {
-  resources,
+const configOptions = {
+  resources: localesResources,
   fallbackLng: 'en',
   supportedLngs: ['en', 'uk'],
   defaultNS: 'translation',
@@ -23,6 +23,6 @@ const options = {
   initImmediate: false,
 };
 
-i18n.use(initReactI18next).init(options);
+i18n.use(initReactI18next).init(configOptions);
 
 export default i18n;

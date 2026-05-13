b2da9394ba8fbfb936f542886bad0cbe15195542
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-14 01:00:14 +0300

    Refactor: add StatausMap type for better clarity and consitency, update types with new type.

--- src/widgets/projects/lib/status-map/status-map.ts
+++ src/widgets/projects/lib/status-map/status-map.ts
@@ -1,8 +1,6 @@
-import type { TabId } from '@/widgets/projects/model/types';
+import type { StatusMap } from '@/widgets/projects/model/types';
 
-import type { ProjectStatus } from '@/features/projects/model/types';
-
-export const statusMap: Record<TabId, ProjectStatus | null> = {
+export const statusMap: StatusMap = {
   allProjects: null,
   inProgress: 'In Progress',
   pending: 'Pending',
--- src/widgets/projects/model/types.ts
+++ src/widgets/projects/model/types.ts
@@ -1,4 +1,7 @@
 import type { ComponentType } from 'react';
 
+import type { ProjectStatus } from '@/features/projects/model/types';
+
 export type TabId = 'allProjects' | 'inProgress' | 'pending' | 'completed' | 'canceled';
 export type TabsContent = Record<TabId, ComponentType>;
+export type StatusMap = Record<TabId, ProjectStatus | null>;

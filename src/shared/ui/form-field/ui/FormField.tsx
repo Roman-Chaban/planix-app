9ce340ba661c2f28f44ad24a51e791455c366b3f
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 23:37:26 +0300

    Feat: Add buildClassName for FormField, add styles for field.

--- src/shared/ui/form-field/ui/FormField.tsx
+++ src/shared/ui/form-field/ui/FormField.tsx
@@ -1,8 +1,10 @@
 import type { FC } from 'react';
 
+import { buildClassName } from '@/shared/lib/helpers/buildClassName/buildClassName';
 import type { FormFieldProps } from '@/shared/ui/form-field/model/types';
+import styles from '@/shared/ui/form-field/styles/form-field.module.scss';
 import { Box } from '@/shared/ui/index';
 
 export const FormField: FC<FormFieldProps> = ({ children, className }) => {
-  return <Box className={className}>{children}</Box>;
+  return <Box className={buildClassName(className, styles.field)}>{children}</Box>;
 };

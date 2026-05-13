d90e432a24667b7d5547572c6d12f3abd8bdddc9
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 23:44:51 +0300

    Feat: Implement project table with status badges and filtering logic.

--- src/widgets/projects/ui/ProjectsLayout.tsx
+++ src/widgets/projects/ui/ProjectsLayout.tsx
@@ -1,21 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
+import { filterProjects } from '@/widgets/projects/lib/filter-projects/filter-projects';
 import { headerItems } from '@/widgets/projects/lib/header-items/header-items';
-import { tabsContent } from '@/widgets/projects/lib/tabs-content/tabs-content';
 import type { TabId } from '@/widgets/projects/model/types';
 import styles from '@/widgets/projects/styles/projects.module.scss';
 import { ProjectsHeader } from '@/widgets/projects/ui/ProjectsHeader/ui/ProjectsHeader';
 import { ProjectToolbar } from '@/widgets/projects/ui/ProjectsToolbar/ui/ProjectsToolbar';
+import { useAllProjects } from '@/features/projects/hooks/useAllProjects';
 import { useProjectsFilters } from '@/features/projects/hooks/useProjectsFilters';
-import { Box, Header, PageWrapper } from '@/shared/ui';
+import { toProjectTableItem } from '@/features/projects/model/adapters';
+import { Box, Header, Modal, NoProjects, PageWrapper } from '@/shared/ui';
+import { ProjectsTable } from '@/shared/ui/project/ui/ProjectTable';
 
 export const ProjectsLayout = () => {
   const [activeId, setActiveId] = useState<TabId>(headerItems[0].id);
-  const { control } = useProjectsFilters();
+  const [projectToDelete, setProjectToDelete] = useState<string | null>(null);
 
-  const ActiveTabComponent = tabsContent[activeId];
+  const { control, watch } = useProjectsFilters();
+  const { data } = useAllProjects();
+
+  const search = watch('search');
+
+  const projectItems = useMemo(() => {
+    return data?.data?.map(toProjectTableItem) ?? [];
+  }, [data]);
+
+  const filteredProjects = useMemo(() => {
+    return filterProjects(projectItems, activeId, search);
+  }, [projectItems, activeId, search]);
+
+  const isEmpty = filteredProjects.length === 0;
 
   return (
     <PageWrapper header={<Header title={'DashboardHeaders.projects'} />}>
@@ -23,7 +39,16 @@ export const ProjectsLayout = () => {
         <ProjectsHeader activeId={activeId} setActiveId={setActiveId} />
         <ProjectToolbar control={control} />
       </Box>
-      <ActiveTabComponent />
+
+      {isEmpty ? (
+        <NoProjects />
+      ) : (
+        <ProjectsTable onTrashClick={(id) => setProjectToDelete(id)} projects={filteredProjects} />
+      )}
+
+      <Modal isOpen={!!projectToDelete} onClose={() => setProjectToDelete(null)}>
+        Modal
+      </Modal>
     </PageWrapper>
   );
 };

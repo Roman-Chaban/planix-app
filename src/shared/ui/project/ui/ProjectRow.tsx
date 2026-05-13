03d4d66451c56075799923f3c54d0ff4d22a11aa
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 23:42:59 +0300

    Feat: Add ActionsBar component, mock Avatar visible.

--- src/shared/ui/project/ui/ProjectRow.tsx
+++ src/shared/ui/project/ui/ProjectRow.tsx
@@ -1,17 +1,20 @@
-import type { FC } from 'react';
+import { type FC } from 'react';
 
-import type { ProjectRowProps } from '@/features/projects/model/types';
 
 import { CalendarIcon } from '@/shared/ui/icons/index';
-import { StatusBadge } from '@/shared/ui/index';
+import { StatusBadge, ActionsBar } from '@/shared/ui/index';
+import type { ProjectRowProps } from '@/shared/ui/project/model/types';
 import styles from '@/shared/ui/project/styles/project.module.scss';
 
-export const ProjectRow: FC<ProjectRowProps> = ({ project }) => {
+export const ProjectRow: FC<ProjectRowProps> = ({ project, onTrashClick }) => {
   const { status, client, name, dueDate, platform, progressText, formattedPrice } = project;
 
   return (
     <tr className={styles.bodyRow}>
-      <td className={styles.cell}>{client.name}</td>
+      <td className={styles.cell}>
+        {/* <Avatar width={40} height={40} src={client.avatar} alt={client.name} /> */}
+        {client.name}
+      </td>
       <td className={styles.cell}>{name}</td>
       <td className={`${styles.cell} ${styles.calendarCell}`}>
         <CalendarIcon />
@@ -23,7 +26,9 @@ export const ProjectRow: FC<ProjectRowProps> = ({ project }) => {
       <td className={styles.cell}>
         <StatusBadge status={status} />
       </td>
-      <td className={styles.cell}>Actions</td>
+      <td className={styles.cell}>
+        <ActionsBar onTrashClick={onTrashClick} />
+      </td>
     </tr>
   );
 };

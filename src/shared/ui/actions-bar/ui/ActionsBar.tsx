69a1dc91c054c2674ca19959b26ae6a5742a3caa
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-13 23:36:54 +0300

    Feat: Add ActionsBar and its styles.

---
+++ src/shared/ui/actions-bar/ui/ActionsBar.tsx
@@ -0,0 +1,22 @@
+import type { FC } from 'react';
+
+import { createActions } from '@/shared/ui/actions-bar/lib/actions';
+import type { ActionsBarProps } from '@/shared/ui/actions-bar/model/types';
+import styles from '@/shared/ui/actions-bar/styles/actions-bar.module.scss';
+import { Box, Button } from '@/shared/ui/index';
+
+export const ActionsBar: FC<ActionsBarProps> = ({ onTrashClick }) => {
+  const actions = createActions(onTrashClick);
+
+  return (
+    <Box className={styles.actions}>
+      {actions.map(({ id, icon: Icon, onClick }) => {
+        return (
+          <Button key={id} className={styles.action} onClick={onClick}>
+            <Icon />
+          </Button>
+        );
+      })}
+    </Box>
+  );
+};

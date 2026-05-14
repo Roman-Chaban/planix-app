cc3657555590c59fe8ca72c9cf0fc801fb48e3e3
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-14 22:08:10 +0300

    Feat: add ModalActions export to index.

--- src/shared/ui/modal/index.ts
+++ src/shared/ui/modal/index.ts
@@ -1,3 +1,4 @@
 export { ModalContent } from '@/shared/ui/modal/ui/ModalContent';
 export { ModalHeader } from '@/shared/ui/modal/ui/ModalHeader';
 export { ModalOverlay } from '@/shared/ui/modal/ui/ModalOverlay';
+export { ModalActions } from '@/shared/ui/modal/ui/ModalActions';

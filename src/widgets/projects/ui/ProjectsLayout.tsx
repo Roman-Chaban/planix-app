ac33156894e37b76abf6547f52b25a7e69c5fd83
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-14 22:15:24 +0300

    Feat: add delete project functionality and project confirmation modal

--- src/widgets/projects/ui/ProjectsLayout.tsx
+++ src/widgets/projects/ui/ProjectsLayout.tsx
@@ -2,6 +2,8 @@
 
 import { useMemo, useState } from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 import { filterProjects } from '@/widgets/projects/lib/filter-projects/filter-projects';
 import { headerItems } from '@/widgets/projects/lib/header-items/header-items';
 import type { TabId } from '@/widgets/projects/model/types';
@@ -11,10 +13,19 @@ import { ProjectToolbar } from '@/widgets/projects/ui/ProjectsToolbar/ui/Project
 import { useAllProjects } from '@/features/projects/hooks/useAllProjects';
 import { useProjectsFilters } from '@/features/projects/hooks/useProjectsFilters';
 import { toProjectTableItem } from '@/features/projects/model/adapters';
-import { Box, Header, Modal, NoProjects, PageWrapper } from '@/shared/ui';
+import { BUTTON_SIZES, BUTTON_TYPES, BUTTON_VARIANTS } from '@/shared/constants/buttons';
+import { Box, Button, Header, Modal, NoProjects, PageWrapper } from '@/shared/ui';
+import { CloseIcon } from '@/shared/ui/icons';
+import { ModalHeader, ModalActions } from '@/shared/ui/modal';
 import { ProjectsTable } from '@/shared/ui/project/ui/ProjectTable';
+import { Textarea } from '@/shared/ui/textarea/ui/Textarea';
+
+const { DEFAULT, OUTLINE } = BUTTON_VARIANTS;
+const { BUTTON } = BUTTON_TYPES;
+const { LARGE } = BUTTON_SIZES;
 
 export const ProjectsLayout = () => {
+  const { t } = useTranslation();
   const [activeId, setActiveId] = useState<TabId>(headerItems[0].id);
   const [projectToDelete, setProjectToDelete] = useState<string | null>(null);
 
@@ -31,7 +42,15 @@ export const ProjectsLayout = () => {
     return filterProjects(projectItems, activeId, search);
   }, [projectItems, activeId, search]);
 
-  const isEmpty = filteredProjects.length === 0;
+  const isEmptyProjects = filteredProjects.length === 0;
+
+  const handleCloseModal = () => {
+    return setProjectToDelete(null);
+  };
+
+  const handleTrashClick = (id: string) => {
+    return setProjectToDelete(id);
+  };
 
   return (
     <PageWrapper header={<Header title={'DashboardHeaders.projects'} />}>
@@ -40,13 +59,35 @@ export const ProjectsLayout = () => {
         <ProjectToolbar control={control} />
       </Box>
 
-      {isEmpty ? (
+      {isEmptyProjects ? (
         <NoProjects />
       ) : (
-        <ProjectsTable onTrashClick={(id) => setProjectToDelete(id)} projects={filteredProjects} />
+        <ProjectsTable onTrashClick={handleTrashClick} projects={filteredProjects} />
       )}
 
-      <Modal isOpen={!!projectToDelete} onClose={() => setProjectToDelete(null)} />
+      <Modal isOpen={!!projectToDelete} onClose={handleCloseModal}>
+        <ModalHeader
+          onClose={handleCloseModal}
+          title="ProjectsModal.Header.title"
+          icon={<CloseIcon />}
+        />
+
+        <Textarea
+          placeholder="ProjectsModal.Content.placeholder"
+          label="ProjectsModal.Content.label"
+          textareaClassName={styles.textarea}
+          labelClassName={styles.reasonLabel}
+        />
+
+        <ModalActions className={styles.actions}>
+          <Button fullWidth type={BUTTON} variant={OUTLINE} size={LARGE} className={styles.cancel}>
+            {t('ProjectsModal.Content.cancel')}
+          </Button>
+          <Button fullWidth type={BUTTON} variant={DEFAULT} size={LARGE} className={styles.save}>
+            {t('ProjectsModal.Content.save')}
+          </Button>
+        </ModalActions>
+      </Modal>
     </PageWrapper>
   );
 };

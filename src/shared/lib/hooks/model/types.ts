58fe1d4d4c13eceb245b4235f52dcd300cc52ea8
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-19 20:09:09 +0300

    Refactor: change types naming, add new types for useDragScroll

---
+++ src/shared/lib/hooks/model/types.ts
@@ -0,0 +1,16 @@
+export type Point = { x: number; y: number };
+export type ScrollPosition = { left: number; top: number };
+export type Axis = 'x' | 'y' | 'both';
+
+export type UseEscapeKeyParams = {
+  enabled: boolean;
+  onEscape: () => void;
+};
+
+export type UseDragScrollOptions = {
+  axis?: Axis;
+  disabled?: boolean;
+  momentum?: boolean;
+  scrollSpeed?: number;
+  dragThreshold?: number;
+};

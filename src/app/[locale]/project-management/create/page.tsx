31f1ad70fc6caf71f0e0ccf3ea5ed5daf8d48799
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-20 18:16:44 +0300

    Feat: create ProjectManagementCreate page

---
+++ src/app/[locale]/project-management/create/page.tsx
@@ -0,0 +1,5 @@
+import { ProjectManagementCreatePage } from '@/views/project-management-create/ui/ProjectManagementCreatePage';
+
+export default function Page() {
+  return <ProjectManagementCreatePage />;
+}

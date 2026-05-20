a9f54bd849cb9b06211b478bf56dc699bb121247
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-20 18:20:09 +0300

    Feat: add sharedI18n translation object keys, add types

---
+++ src/shared/lib/i18n/model/types.ts
@@ -0,0 +1,13 @@
+export type SharedI18n = {
+  sidebar: {
+    dashboard: string;
+    projects: string;
+  };
+  projectHeader: {
+    title: string;
+    metaInfo: {
+      label: string;
+      value: string;
+    };
+  };
+};

b523db994756b55e992984a4b24e346f157dacda
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-20 20:01:13 +0300

    Feat: add ProjectTable skeleton, its styles, constants, add index centralized export file

---
+++ src/shared/ui/skeletons/index.ts
@@ -0,0 +1,1 @@
+export { ProjectsTableSkeleton } from '@/shared/ui/skeletons/project-table/ui/ProjectTableSkeleton';
---
+++ src/shared/ui/skeletons/project-table/model/constants.ts
@@ -0,0 +1,11 @@
+export const columns = [
+  'colClient',
+  'colName',
+  'colDueDate',
+  'colPlatform',
+  'colProgress',
+  'colPrice',
+  'colReason',
+  'colStatus',
+  'colActions',
+] as const;

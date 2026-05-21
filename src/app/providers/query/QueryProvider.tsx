520c3dd932db47dcf340ad0cd6c229326046954d
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-21 19:21:29 +0300

    Refactor: change to better import path

--- src/app/providers/query/QueryProvider.tsx
+++ src/app/providers/query/QueryProvider.tsx
@@ -5,7 +5,7 @@ import { useState, type ReactNode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { createQueryClient } from '@/app/providers/query/lib/createQueryClient';
+import { createQueryClient } from './lib/createQueryClient';
 
 type QueryProviderProps = {
   children: ReactNode;

7b442f1d5951161b5e243ee389f744cf125da57c
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-21 19:13:54 +0300

    Refactor: update index, rename AppProvider to AppProviders for better naming

--- src/app/providers/core/AppProviders.tsx
+++ src/app/providers/core/AppProviders.tsx
@@ -1,15 +1,15 @@
 import type { FC, ReactNode } from 'react';
 
-import { I18nProvider } from '@/app/providers/i18n/I18nProvider';
-import { QueryProvider } from '@/app/providers/query/QueryProvider';
+import { I18nProvider } from '@/app/providers/i18n';
+import { QueryProvider } from '@/app/providers/query';
 import type { Locale } from '@/shared/lib/i18n/locales';
 
-type AppProviderProps = {
+type AppProvidersProps = {
   children: ReactNode;
   locale: Locale;
 };
 
-export const AppProvider: FC<AppProviderProps> = ({ children, locale }) => {
+export const AppProviders: FC<AppProvidersProps> = ({ children, locale }) => {
   return (
     <QueryProvider>
       <I18nProvider locale={locale}>{children}</I18nProvider>
---
+++ src/app/providers/index.ts
@@ -0,0 +1,1 @@
+export { AppProviders } from './core/AppProviders';

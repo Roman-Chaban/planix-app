63c6d7fed739edc06abe63222cd342a955e6dc84
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-24 20:28:21 +0300

    Refactor: update reset styles import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import { DM_Sans } from 'next/font/google';
 
-import '@shared/styles/reset/_reset.scss';
+import '@shared/styles/base/index.scss';
 
 const dm_sans = DM_Sans({
   weight: ['400', '500', '600', '700', '800'],

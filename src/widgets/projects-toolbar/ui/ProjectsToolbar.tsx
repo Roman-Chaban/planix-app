5b0ea5ce0dafa59bfe775562a83f6dce738bddca
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-27 17:53:34 +0300

    Refactor: change search logic , add InputField, update props, delete useless imports

--- src/widgets/projects-toolbar/ui/ProjectsToolbar.tsx
+++ src/widgets/projects-toolbar/ui/ProjectsToolbar.tsx
@@ -1,38 +1,36 @@
 'use client';
 
 import type { FC } from 'react';
-
 import { useTranslation } from 'react-i18next';
 
 import { useProjectToolbar } from '@/widgets/projects-toolbar/lib/useProjectToolbar';
 import type { ProjectsToolbarProps } from '@/widgets/projects-toolbar/model/types';
 
-import { ICON_POSITION } from '@/shared/constants';
-import { Box, ProjectButton, Input, FormField } from '@/shared/ui';
-import { FormIcon, FormWrapper } from '@/shared/ui/form-field';
+import { Box, ProjectButton } from '@/shared/ui';
+import { InputField } from '@/shared/ui/form-field';
 import { SearchIconPrimary } from '@/shared/ui/icons';
 import { INPUT_TYPES } from '@/shared/ui/input/model/constants';
 
 import styles from './ProjectsToolbar.module.scss';
 
-const { START } = ICON_POSITION;
 const { SEARCH } = INPUT_TYPES;
 
 export const ProjectToolbar: FC<ProjectsToolbarProps> = () => {
   const { t } = useTranslation('addProjectHeader');
-
   const { handleCreateProject } = useProjectToolbar();
 
   return (
     <Box className={styles.toolbar}>
-      <FormField className={styles.search}>
-        <FormWrapper variant={SEARCH}>
-          <FormIcon position={START}>
-            <SearchIconPrimary />
-          </FormIcon>
-          <Input type={SEARCH} placeholder={t('searchPlaceholder')} />
-        </FormWrapper>
-      </FormField>
+      <Box className={styles.search}>
+        <InputField
+          id="project-search"
+          startIcon={<SearchIconPrimary />}
+          inputProps={{
+            type: SEARCH,
+            placeholder: t('searchPlaceholder'),
+          }}
+        />
+      </Box>
 
       <ProjectButton onClick={handleCreateProject} />
     </Box>

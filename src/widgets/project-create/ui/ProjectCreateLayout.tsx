5f71e1574839c3481ca19dd47f7a724230a65aed
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-27 17:55:29 +0300

    Feat: add ProjectDetails component into layout

--- src/widgets/project-create/ui/ProjectCreateLayout.tsx
+++ src/widgets/project-create/ui/ProjectCreateLayout.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@/widgets/header';
 import { ProjectHeader } from '@/widgets/project-header';
+import { ProjectDetails } from '@/features/project';
 import { PageWrapper } from '@/shared/ui';
 
 export const ProjectCreateLayout = () => {
   return (
     <PageWrapper header={<Header title="projects" />}>
       <ProjectHeader title="title" metaInfo={{ label: 'label', value: 'value' }} />
+
+      <ProjectDetails />
     </PageWrapper>
   );
 };

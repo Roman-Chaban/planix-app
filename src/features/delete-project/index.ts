c5927fa8403b6412979d0d4abb29faa5774759d4
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-28 22:52:55 +0300

    Feat: add project-details page with dynamic slug route, update exports in types

--- src/features/delete-project/index.ts
+++ src/features/delete-project/index.ts
@@ -1,10 +1,12 @@
 /* Types */
 
-export type { ProjectDeleteModalProps } from './model/types';
+export type { ProjectDeleteModalProps } from './lib/types';
+export type { UseDeleteProjectProps } from './lib/types';
 
 /* Hooks */
 
-export { useProjectDeleteModal } from './model/useProjectDeleteModal';
+export { useProjectDeleteModal } from './lib/useProjectDeleteModal';
+export { useDeleteProject } from './lib/useDeleteProject';
 
 /* Components */
 

9bdb0a036c854eeda108043da342a699a7ed3a89
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-28 21:19:14 +0300

    Feat: add PROJECT_DETAILS route to paths for project detail navigation

--- src/app/routes/paths.ts
+++ src/app/routes/paths.ts
@@ -3,6 +3,7 @@ export const ROUTES = {
   LOGIN: '/login',
   DASHBOARD: '/dashboard',
   PROJECT: '/project-management',
+  PROJECT_DETAILS: '/project-details/',
   PROJECT_CREATE: '/project-management/create',
   CLIENTS: '/clients',
   LEADS: '/leads',

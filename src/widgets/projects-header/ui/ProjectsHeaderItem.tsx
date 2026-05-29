c9ab2c8723961ab6d2d8feeb2384803df793868f
Roman Chaban <roman.chaban1001@gmail.com>
2026-05-29 20:33:14 +0300

    Feat: add variant and size for button, delete useless styles rule

--- src/widgets/projects-header/ui/ProjectsHeaderItem.tsx
+++ src/widgets/projects-header/ui/ProjectsHeaderItem.tsx
@@ -7,18 +7,20 @@ import type { ProjectsHeaderItemProps } from '@/widgets/projects-header/model/ty
 
 import { buildClassName } from '@/shared/lib';
 import { Box, Button, Typography } from '@/shared/ui';
-import { BUTTON_TYPES } from '@/shared/ui/button/model/constants';
+import { BUTTON_SIZES, BUTTON_TYPES, BUTTON_VARIANTS } from '@/shared/ui/button/model/constants';
 
 import styles from './ProjectsHeader.module.scss';
 
 const { BUTTON } = BUTTON_TYPES;
+const { COMPACT } = BUTTON_SIZES;
+const { TRANSPARENT } = BUTTON_VARIANTS;
 
 export const ProjectsHeaderItem: FC<ProjectsHeaderItemProps> = ({ label, isActive, onClick }) => {
   const { t } = useTranslation('addProjectHeader');
 
   return (
     <Box className={buildClassName(styles.box, isActive && styles.active)} onClick={onClick}>
-      <Button type={BUTTON} className={styles.button}>
+      <Button type={BUTTON} variant={TRANSPARENT} size={COMPACT}>
         <Typography
           as="span"
           className={buildClassName(styles.label, isActive && styles.activeLabel)}

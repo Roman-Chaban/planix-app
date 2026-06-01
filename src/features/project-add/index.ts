971e91be270ac33e5447895e5810a27d767ea4a3
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-01 19:35:45 +0300

    feat: export ProjectDetailsSchema and projectDetailsSchema from add-project-details

--- src/features/project-add/index.ts
+++ src/features/project-add/index.ts
@@ -2,6 +2,8 @@ export type { ProjectDetailsFormData, ProjectDetailsFormProps } from './model/ty
 
 export { useProjectDetailsForm } from './lib/useProjectDetailsForm';
 
+export { type ProjectDetailsSchema, projectDetailsSchema } from './ui//add-project-details';
+
 export { ProjectDetails } from './ui/add-project-details/ui/ProjectDetails';
 export { DateFormField } from './ui/project-add-fields/ui/DateFormField';
 export { ProjectDetailsForm } from './ui/add-project-details/ui/ProjectDetailsForm';

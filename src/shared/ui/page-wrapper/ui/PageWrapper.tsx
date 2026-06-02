2bf3ed1c3916ac1ce3e47e7f664a05f58db7ee0a
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-02 20:55:25 +0300

    Feat: refactor PageWrapper to enhance sidebar functionality and improve grid layout

--- src/shared/ui/page-wrapper/ui/PageWrapper.tsx
+++ src/shared/ui/page-wrapper/ui/PageWrapper.tsx
@@ -1,19 +1,35 @@
+'use client';
+
 import type { FC } from 'react';
 
-import { Sidebar } from '@/widgets/sidebar';
+import { Sidebar, useSidebar } from '@/widgets/sidebar';
+import { buildClassName } from '@/shared/lib';
 import { Box, Grid } from '@/shared/ui';
 import type { PageWrapperProps } from '@/shared/ui/page-wrapper';
 
 import styles from './PageWrapper.module.scss';
 
 export const PageWrapper: FC<PageWrapperProps> = ({ children, header, sectionClassName }) => {
+  const { isSidebarOpen, toggleSidebar, hydrated } = useSidebar();
+
+  if (!hydrated) {
+    return null;
+  }
+
   return (
-    <Grid className={styles.grid}>
-      <Sidebar />
+    <Grid
+      className={buildClassName(styles.grid, {
+        [styles.collapsed]: !isSidebarOpen,
+      })}
+    >
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
       {header}
 
-      <Box as="main" className={styles.main}>
+      <Box
+        as="main"
+        className={buildClassName(styles.main, { [styles.mainCollapsed]: !isSidebarOpen })}
+      >
         <Box as="section" className={sectionClassName}>
           {children}
         </Box>

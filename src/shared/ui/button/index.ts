4d8c373da0b30214e295b78b4a6a6bbe91538bb6
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-02 20:55:40 +0300

    Feat: export button constants from model for improved accessibility

--- src/shared/ui/button/index.ts
+++ src/shared/ui/button/index.ts
@@ -1,3 +1,5 @@
 export type { ButtonProps } from './model/types';
 
+export { BUTTON_SHAPES, BUTTON_SIZES, BUTTON_TYPES, BUTTON_VARIANTS } from './model/constants';
+
 export { Button } from './ui/Button';

f09738ae765dd6191e75a85d13950f6050946dd8
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-05 18:30:49 +0300

    Feat: update Project and Client types to include RejectReason and ProjectFile definitions

--- src/entities/project/model/types.ts
+++ src/entities/project/model/types.ts
@@ -1,10 +1,22 @@
-import type { Client, RejectReason } from '@/widgets/project-table';
+import type { PROJECT_REASONS } from '@/widgets/project-table';
 import { type ProjectDetailsFormData } from '@/features/project-add';
 import type { Status } from '@/shared/ui/status-badge/model/types';
 
 export type CreateProjectPayload = ProjectDetailsFormData;
 export type UpdateProjectPayload = { id: string | number; data: Partial<ProjectDetailsFormData> };
 export type DeleteProjectPayload = { id: string | number };
+export type RejectReason = (typeof PROJECT_REASONS)[keyof typeof PROJECT_REASONS];
+
+export type Client = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+export type ProjectFile = {
+  name: string;
+  url: string;
+};
 
 export type Project = {
   id: string | number;
@@ -22,4 +34,5 @@ export type Project = {
   slug: string;
   name_uk?: string;
   description_uk?: string;
+  files: ProjectFile[];
 };

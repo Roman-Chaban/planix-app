344b5236fd55b1c26efd0c80e1c0e4e36b4c9c1e
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-08 23:06:16 +0300

    Feat: simplify project table types by removing unnecessary fields and aligning with Project type

--- src/widgets/project-table/model/types.ts
+++ src/widgets/project-table/model/types.ts
@@ -1,9 +1,8 @@
-import type { Project, ProjectFile, RejectReason, Status } from '@/entities/project/model/types';
+import type { Project } from '@/entities/project/model/types';
 import type { ActionItem } from '@/shared/ui/actions-bar/model/types';
-import type { StatusColor } from '@/shared/ui/status-badge/model/types';
 
 export type TableColumn = {
-  key: keyof ProjectTableItem | 'actions';
+  key: keyof Project | 'actions';
   label: string;
 };
 
@@ -21,42 +20,14 @@ export type ProjectsResponse = {
   };
 };
 
-export type ProjectDBRow = {
-  id: string | number;
-  project_name: string;
-  client_id: string;
-  client_name: string;
-  client_avatar: string;
-  start_date: string;
-  due_date: string;
-  platform: string;
-  progress: number;
-  price: number;
-  status: Status;
-  reason: RejectReason;
-  slug: string;
-  description: string;
-  description_uk: string;
-  name_uk: string;
-  files: ProjectFile[];
-};
-
-export type ProjectTableItem = {
-  progressText: string;
-  statusColor: StatusColor;
-  formattedPrice: string;
-  formattedDueDate: string;
-  reason: RejectReason;
-} & Project;
-
 export type ProjectTableRowProps = {
-  project: ProjectTableItem;
+  project: Project;
   actionsFactory: (name: string, id: string | number) => ActionItem[];
   isShowReason?: boolean;
 };
 
 export type ProjectsTableProps = {
-  projects: ProjectTableItem[];
+  projects: Project[];
   onDelete: (id: string | number) => void;
   isShowReason?: boolean;
 };

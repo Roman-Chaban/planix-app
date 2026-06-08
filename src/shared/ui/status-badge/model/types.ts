e5ee71f928843389f034a7af8655dc8e5bf00fed
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-08 15:49:17 +0300

    Feat: update Status type definition for improved clarity and consistency

--- src/shared/ui/status-badge/model/types.ts
+++ src/shared/ui/status-badge/model/types.ts
@@ -1,8 +1,8 @@
 import type { FC, SVGProps } from 'react';
 
 import type { TabId } from '@/widgets/project-layout/model/types';
+import type { Status } from '@/entities/project';
 
-export type Status = 'Pending' | 'In Progress' | 'Completed' | 'Canceled';
 export type StatusMap = Record<TabId, Status | null>;
 export type StatusClasses = Record<Status, string>;
 export type StatusIcons = Record<Status, FC<SVGProps<SVGSVGElement>>>;

6a027c934d477e4d517b4430562fe0116bc0cc5b
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-09 18:22:36 +0300

    Refactor: update Project type to use string for progress and price fields, remove redundant name_uk field

--- src/entities/project/model/types.ts
+++ src/entities/project/model/types.ts
@@ -18,21 +18,21 @@ export type ProjectFile = {
 export type Project = {
   id: string | number;
   project_name: string;
+  project_name_uk: string;
   client_id: string;
   client_name: string;
   client_avatar: string;
   start_date: string;
   due_date: string;
   platform: string;
-  progress: number;
-  price: number;
+  progress: string;
+  price: string;
   status: Status;
   reason: RejectReason;
   slug: string;
   description: string;
   description_uk: string;
-  name_uk: string;
-  files: ProjectFile[] | File[];
+  files: (ProjectFile | File)[];
 };
 
 export type CreateProjectPayload = ProjectDetailsFormData;

48ef0bfd1c88117a34215fdb02153fcedaddeb94
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-09 18:22:56 +0300

    Refactor: simplify mapCreateProject function by removing unused parameters and fields

--- src/entities/project/api/lib/project.mappers.ts
+++ src/entities/project/api/lib/project.mappers.ts
@@ -1,26 +1,21 @@
 import type { ProjectFormData } from '@/features/project-add/model/types';
-import type { UpdateProjectPayload, Project, ProjectFile } from '@/entities/project';
+import type { UpdateProjectPayload, Project } from '@/entities/project';
 
-export const mapCreateProject = (
-  formData: ProjectFormData,
-  processedFiles: ProjectFile[],
-): Partial<Project> => {
+export const mapCreateProject = (formData: ProjectFormData): Partial<Project> => {
   return {
     project_name: formData.projectName,
-    name_uk: formData.projectName,
     client_id: String(Date.now()),
     client_name: formData.clientName,
     client_avatar: '',
     start_date: formData.startDate,
     due_date: formData.dueDate,
     platform: formData.platform,
-    price: Number(formData.price) || 0,
-    progress: 0,
+    price: formData.price,
+    progress: '0',
     status: 'Pending',
     description: formData.description,
-    description_uk: formData.description,
     slug: formData.projectName.toLowerCase().replace(/ /g, '-'),
-    files: processedFiles,
+    files: formData.files,
   };
 };
 
@@ -28,7 +23,7 @@ export const mapUpdateProject = (data: UpdateProjectPayload) => {
   return {
     name: data.data.projectName,
     dueDate: data.data.dueDate,
-    price: Number(data.data.price),
+    price: data.data.price,
     platform: data.data.platform,
   };
 };

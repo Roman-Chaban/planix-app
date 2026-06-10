7774e33cb321e87e2c07f591a9f18c5161dc04e5
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-10 16:30:42 +0300

    Feat: reorganize imports in PageWrapper component for better clarity

--- src/shared/ui/page-wrapper/ui/PageWrapper.tsx
+++ src/shared/ui/page-wrapper/ui/PageWrapper.tsx
@@ -2,7 +2,8 @@
 
 import type { FC } from 'react';
 
-import { Sidebar, useSidebar } from '@/widgets/sidebar';
+import { Sidebar } from '@/widgets/sidebar';
+import { useSidebar } from '@/features/sidebar';
 import { buildClassName } from '@/shared/lib';
 import { Box, Grid } from '@/shared/ui';
 import type { PageWrapperProps } from '@/shared/ui/page-wrapper';

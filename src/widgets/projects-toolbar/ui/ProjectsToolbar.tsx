e0bfabff2234896d6da01168a05e5ae3b600654e
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-10 16:30:27 +0300

    Feat: enhance ProjectsToolbar responsiveness and sidebar integration

--- src/widgets/projects-toolbar/ui/ProjectsToolbar.tsx
+++ src/widgets/projects-toolbar/ui/ProjectsToolbar.tsx
@@ -6,16 +6,25 @@ import { ProjectHeader, ProjectActions } from '@/widgets/projects-toolbar';
 import { useProjectToolbar } from '@/widgets/projects-toolbar/lib/useProjectToolbar';
 import type { ProjectToolbarProps } from '@/widgets/projects-toolbar/model/types';
 
+import { useSidebar } from '@/features/sidebar';
+import { buildClassName } from '@/shared/lib';
 import { Box } from '@/shared/ui';
 
 import styles from './ProjectsToolbar.module.scss';
 
 export const ProjectToolbar: FC<ProjectToolbarProps> = ({ activeId, setActiveId }) => {
   const { handleCreateProject } = useProjectToolbar();
+  const { isSidebarOpen } = useSidebar();
 
   return (
-    <Box className={styles.toolbar}>
-      <ProjectHeader activeId={activeId} setActiveId={setActiveId} />
+    <Box
+      className={buildClassName(
+        styles.toolbar,
+        { [styles.toolbarCollapsed]: !isSidebarOpen },
+        { [styles.toolbarExpanded]: isSidebarOpen },
+      )}
+    >
+      <ProjectHeader activeId={activeId} setActiveId={setActiveId} isSidebarOpen={isSidebarOpen} />
       <ProjectActions handleCreateProject={handleCreateProject} />
     </Box>
   );

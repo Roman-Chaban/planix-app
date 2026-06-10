3903089aa95b0f69deaab6116028574480a728f3
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-10 23:12:27 +0300

    Feat: replace LoginLayout with AuthLayout and LoginForm for improved authentication page structure

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
-import { LoginLayout } from '@/widgets/login';
+import { LoginForm } from '@/features/auth-by-credentials/ui/LoginForm';
+
+import { AuthLayout } from '@/shared/ui';
 
 export default function LoginPage() {
-  return <LoginLayout />;
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  );
 }

880feec8bb77c7ed80fb61646ab3abbac5084248
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-11 19:40:56 +0300

    Feat: fix import path for LoginForm component in LoginPage

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from '@/features/auth-by-credentials/ui/LoginForm';
+import { LoginForm } from '@/features/auth-by-credentials/ui/login/LoginForm';
 
 import { AuthLayout } from '@/shared/ui';
 

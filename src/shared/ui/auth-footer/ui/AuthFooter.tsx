610479beeb565b180f79a46f79621949ee65e8f6
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-11 19:35:14 +0300

    Feat: add AuthFooter component with styles and props for authentication flow

---
+++ src/shared/ui/auth-footer/ui/AuthFooter.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react';
+
+import { ROUTES } from '@/app/routes';
+import { Box, AppLink, Typography } from '@/shared/ui';
+import type { AuthFooterProps } from '@/shared/ui/auth-footer';
+
+import styles from './AuthFooter.module.scss';
+
+const { SIGN_UP } = ROUTES;
+
+export const AuthFooter: FC<AuthFooterProps> = ({ title, link }) => {
+  return (
+    <Box className={styles.footer}>
+      <Typography as="span" className={styles.text}>
+        {title}
+      </Typography>
+
+      <AppLink href={SIGN_UP} className={styles.link}>
+        {link}
+      </AppLink>
+    </Box>
+  );
+};

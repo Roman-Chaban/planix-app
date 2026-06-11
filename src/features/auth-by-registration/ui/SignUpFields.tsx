8c644b2a142e5b2372d3342f3dc95778274414b7
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-12 00:18:32 +0300

    Feat: enhance SignUpFields with internationalization and additional input fields

--- src/features/auth-by-registration/ui/SignUpFields.tsx
+++ src/features/auth-by-registration/ui/SignUpFields.tsx
@@ -4,6 +4,7 @@ import type { FC } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
+import { useSignUp } from '@/features/auth-by-registration';
 import { AuthButton, Box, FormInputField } from '@/shared/ui';
 
 import {
@@ -15,57 +16,100 @@ import {
   ContactIcon,
 } from '@/shared/ui/icons';
 
+import { INPUT_TYPES } from '@/shared/ui/input';
+
 import styles from './SignUp.module.scss';
+
+const { EMAIL, PASSWORD, TEXT } = INPUT_TYPES;
 
 export const SignUpFields: FC = () => {
   const { t } = useTranslation('signUpForm');
+
+  const { register, errors } = useSignUp();
 
   return (
     <>
       <FormInputField
         id="fullName"
-        label="Full Name"
-        inputProps={{ placeholder: 'Enter Full Name' }}
+        label={t('fullName')}
         startIcon={<ClientIcon width={20} height={20} />}
+        error={errors.fullName?.message}
+        inputProps={{
+          ...register('fullName', {}),
+          type: TEXT,
+          placeholder: t('fullNamePlaceholder'),
+          autoComplete: 'name',
+        }}
       />
+
       <FormInputField
         id="email"
-        label="Email"
-        inputProps={{ placeholder: 'Enter Email' }}
+        label={t('email')}
         startIcon={<MessageIcon />}
+        error={errors.email?.message}
+        inputProps={{
+          ...register('email', {}),
+          type: EMAIL,
+          placeholder: t('emailPlaceholder'),
+          autoComplete: 'email',
+        }}
       />
 
       <Box className={styles.box}>
         <FormInputField
           id="password"
-          label="Password"
-          inputProps={{ placeholder: 'Enter Password' }}
+          label={t('password')}
           startIcon={<LockIcon />}
           endIcon={<ViewOffIcon />}
+          error={errors.password?.message}
+          inputProps={{
+            ...register('password', {}),
+            type: PASSWORD,
+            placeholder: t('passwordPlaceholder'),
+            autoComplete: 'new-password',
+          }}
         />
 
         <FormInputField
           id="confirmPassword"
-          label="Confirm Password"
-          inputProps={{ placeholder: 'Confirm Password' }}
+          label={t('confirmPassword')}
           startIcon={<LockIcon />}
           endIcon={<ViewOffIcon />}
+          error={errors.confirmPassword?.message}
+          inputProps={{
+            ...register('confirmPassword', {}),
+            type: PASSWORD,
+            placeholder: t('confirmPasswordPlaceholder'),
+            autoComplete: 'new-password',
+          }}
         />
       </Box>
 
       <Box className={styles.box}>
         <FormInputField
           id="contact"
-          label="Contact No"
+          label={t('contact')}
           startIcon={<ContactIcon />}
-          inputProps={{ placeholder: 'Enter Contact No' }}
+          error={errors.contact?.message}
+          inputProps={{
+            ...register('contact'),
+            type: TEXT,
+            placeholder: t('contactPlaceholder'),
+            autoComplete: 'tel',
+          }}
         />
 
         <FormInputField
           id="birthDate"
-          label="Date of Birth"
+          label={t('birthDate')}
           startIcon={<CalendarIcon />}
-          inputProps={{ placeholder: 'Select Date of Birth' }}
+          error={errors.birthDate?.message}
+          inputProps={{
+            ...register('birthDate'),
+            type: TEXT,
+            placeholder: t('birthDatePlaceholder'),
+            autoComplete: 'bday',
+          }}
         />
       </Box>
 

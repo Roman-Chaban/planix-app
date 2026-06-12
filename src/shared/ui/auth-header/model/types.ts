74eed74a384112aae5268adc57b21194b4faedca
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-12 18:48:33 +0300

    Feat: enhance AuthHeader component to conditionally render highlighted icon based on isHighlightedIcon prop

--- src/shared/ui/auth-header/model/types.ts
+++ src/shared/ui/auth-header/model/types.ts
@@ -1,5 +1,6 @@
 export type AuthHeaderProps = {
   title: string;
   highlightedText?: string;
+  isHighlightedIcon?: boolean;
   subtitle?: string;
 };
--- src/shared/ui/auth-header/ui/AuthHeader.tsx
+++ src/shared/ui/auth-header/ui/AuthHeader.tsx
@@ -6,13 +6,18 @@ import type { AuthHeaderProps } from '@/shared/ui/auth-header';
 
 import styles from './AuthHeader.module.scss';
 
-export const AuthHeader: FC<AuthHeaderProps> = ({ title, highlightedText, subtitle }) => {
+export const AuthHeader: FC<AuthHeaderProps> = ({
+  title,
+  highlightedText,
+  isHighlightedIcon,
+  subtitle,
+}) => {
   return (
     <Box className={styles.header}>
       <Typography as="h1" className={styles.title}>
         {title}{' '}
         <Typography as="span" className={styles.highlightedText}>
-          {highlightedText} 👋
+          {highlightedText} {isHighlightedIcon && '👋'}
         </Typography>
       </Typography>
 

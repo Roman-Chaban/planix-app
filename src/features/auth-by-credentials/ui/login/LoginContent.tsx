312d47996461ca6f99734fb158ae9cc5f49ec968
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-12 18:24:09 +0300

    Feat: update LoginContent to use dynamic FORGOT_PASSWORD route in AppLink

--- src/features/auth-by-credentials/ui/login/LoginContent.tsx
+++ src/features/auth-by-credentials/ui/login/LoginContent.tsx
@@ -5,12 +5,15 @@ import { Controller } from 'react-hook-form';
 
 import { useTranslation } from 'react-i18next';
 
+import { ROUTES } from '@/app/routes';
 import { useLogin } from '@/features/auth-by-credentials';
 import type { LoginContentProps } from '@/features/auth-by-credentials/model/types';
 
 import { Box, Checkbox, AppLink, AuthButton } from '@/shared/ui';
 
 import styles from './LoginForm.module.scss';
+
+const { FORGOT_PASSWORD } = ROUTES;
 
 export const LoginContent: FC<LoginContentProps> = ({ control }) => {
   const { t } = useTranslation('login');
@@ -28,8 +31,7 @@ export const LoginContent: FC<LoginContentProps> = ({ control }) => {
           )}
         />
 
-        {/* TODO: [Added the correct link when it will need] */}
-        <AppLink href="/#" className={styles.loginFormLink}>
+        <AppLink href={FORGOT_PASSWORD} className={styles.loginFormLink}>
           {t('forgotPassword')}
         </AppLink>
       </Box>

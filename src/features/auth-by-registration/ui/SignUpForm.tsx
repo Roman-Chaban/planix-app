6cbde74ff16fc801df227e3ec134d75a8ee2b442
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-12 19:51:12 +0300

    Feat: integrate AuthRedirect component into SignUpForm footer

--- src/features/auth-by-registration/ui/SignUpForm.tsx
+++ src/features/auth-by-registration/ui/SignUpForm.tsx
@@ -2,9 +2,12 @@
 
 import { useTranslation } from 'react-i18next';
 
+import { ROUTES } from '@/app/routes';
 import { SignUpFields } from '@/features/auth-by-registration';
-import { AuthFooter, AuthHeader } from '@/shared/ui';
+import { AuthFooter, AuthHeader, AuthRedirect } from '@/shared/ui';
 import { AuthWrapper } from '@/shared/ui/auth-wrapper';
+
+const { REGISTER } = ROUTES;
 
 export const SignUpForm = () => {
   const { t } = useTranslation('signUpForm');
@@ -12,7 +15,11 @@ export const SignUpForm = () => {
   return (
     <AuthWrapper
       header={<AuthHeader title={t('title')} subtitle={t('subtitle')} isHighlightedIcon />}
-      footer={<AuthFooter title={t('noAccount')} link={t('registration')} />}
+      footer={
+        <AuthFooter>
+          <AuthRedirect title={t('noAccount')} linkText={t('registration')} href={REGISTER} />
+        </AuthFooter>
+      }
     >
       <SignUpFields />
     </AuthWrapper>

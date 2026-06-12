6d779358d378a82ca46efa7130c4d9bfc54f729c
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-12 20:43:05 +0300

    Feat: remove unnecessary button styling from SignUpFields component

--- src/features/auth-by-registration/ui/SignUpFields.tsx
+++ src/features/auth-by-registration/ui/SignUpFields.tsx
@@ -111,11 +111,7 @@ export const SignUpFields = () => {
         />
       </Box>
 
-      <AuthButton
-        label={t('registration')}
-        disabled={!isValid}
-        className={styles.button}
-      />
+      <AuthButton label={t('registration')} disabled={!isValid} />
     </>
   );
 };

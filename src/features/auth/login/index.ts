ba83aa92c76c9a332e6bd089268758f05d84a5ee
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-13 17:44:06 +0300

    Refactor: delete useless exports from index

--- src/features/auth/login/index.ts
+++ src/features/auth/login/index.ts
@@ -3,9 +3,7 @@ export type { LoginContentProps, RegisterFormValues } from './model/types';
 export { loginSchema, type LoginSchema } from './model/login.schema';
 
 export { useLogin } from './lib/useLogin';
-export { usePasswordToggle } from './lib/usePasswordToggle';
 export { loginFormFields } from './lib/config';
 
 export { LoginForm } from './ui/form/LoginForm';
-export { LoginFields } from './ui/form/LoginFields';
 export { LoginContent } from './ui/form/LoginContent';

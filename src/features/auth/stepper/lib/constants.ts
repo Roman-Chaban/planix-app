29f9008e53b9b4fc568e3bb081b361a373276c01
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-22 23:31:41 +0300

    Feat: comment out VERIFY step in AUTH_STEPS for future implementation

--- src/features/auth/stepper/lib/constants.ts
+++ src/features/auth/stepper/lib/constants.ts
@@ -3,6 +3,8 @@ export const AUTH_STEPS = {
   REGISTER: 'register',
   FORGOT: 'forgot',
   RESET: 'reset',
+  // TODO: [Temporary mock this field]
+  // VERIFY: 'verify',
 } as const;
 
 export type AuthStep = (typeof AUTH_STEPS)[keyof typeof AUTH_STEPS];

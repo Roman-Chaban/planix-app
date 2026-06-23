b4e6e1e4250ffa58da31357d1756be3fe310a770
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-23 16:48:55 +0300

    Feat: enhance useLocalStorage hook to include hydration state

--- src/shared/lib/hooks/useLocalStorage.ts
+++ src/shared/lib/hooks/useLocalStorage.ts
@@ -1,22 +1,35 @@
 'use client';
 
-import { type Dispatch, type SetStateAction, useState } from 'react';
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
 
 export function useLocalStorage<T>(
   key: string,
   initialValue: T,
-): [T, Dispatch<SetStateAction<T>>] {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
+): [T, Dispatch<SetStateAction<T>>, boolean] {
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
+  const [hydrated, setHydrated] = useState<boolean>(false);
 
-  const setValue = (value: T | ((val: T) => T)) => {
-    const valueToStore = value instanceof Function ? value(storedValue) : value;
+  useEffect(() => {
+    try {
+      const item = window.localStorage.getItem(key);
 
-    setStoredValue(valueToStore);
-    localStorage.setItem(key, JSON.stringify(valueToStore));
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      if (item !== null) setStoredValue(JSON.parse(item));
+    } catch {
+    } finally {
+      setHydrated(true);
+    }
+  }, [key]);
+
+  const setValue: Dispatch<SetStateAction<T>> = (value) => {
+    setStoredValue((prev) => {
+      const valueToStore = value instanceof Function ? value(prev) : value;
+
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+
+      return valueToStore;
+    });
   };
 
-  return [storedValue, setValue];
+  return [storedValue, setValue, hydrated];
 }

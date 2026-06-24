39a8ddc735c24d761b1ec15b09e252521f23b9de
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-24 20:37:57 +0300

    Refactor: make Box component more usable with ref attr

--- src/shared/ui/box/ui/Box.tsx
+++ src/shared/ui/box/ui/Box.tsx
@@ -1,17 +1,18 @@
-import { type ElementType, forwardRef } from 'react';
+import type { ElementType } from 'react';
 
 import type { BoxProps } from '@/shared/ui/box';
 
-export const Box = forwardRef<HTMLElement, BoxProps>(
-  ({ as: Component = 'div', children, className, ...props }, ref) => {
-    const Tag = Component as ElementType;
+export const Box = <T extends ElementType = 'div'>({
+  as,
+  children,
+  className,
+  ...boxProps
+}: BoxProps<T>) => {
+  const Component = as || 'div';
 
-    return (
-      <Tag ref={ref} className={className} {...props}>
-        {children}
-      </Tag>
-    );
-  },
-);
-
-Box.displayName = 'Box';
+  return (
+    <Component className={className} {...boxProps}>
+      {children}
+    </Component>
+  );
+};

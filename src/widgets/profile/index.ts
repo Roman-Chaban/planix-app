8ea91a3e97024debeaf8559d23207aa4607990d1
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-25 21:53:29 +0300

    Refactor: export ProfileInfoItem alongside ProfileDetails for improved profile component structure

--- src/widgets/profile/index.ts
+++ src/widgets/profile/index.ts
@@ -1,3 +1,3 @@
 export { ProfileLayout } from './layout';
-export { ProfileDetails } from './details';
+export { ProfileDetails, ProfileInfoItem } from './details';
 export { ProfileMenu } from './menu';

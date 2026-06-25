5c40cc3a5b4746667998848b8b34524da205ee23
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-25 16:49:16 +0300

    Refactor: update Tooltip component to use TOOLTIP_POSITION for default position

--- src/shared/ui/tooltip/ui/Tooltip.tsx
+++ src/shared/ui/tooltip/ui/Tooltip.tsx
@@ -1,15 +1,16 @@
-import type { TooltipProps } from '../model/types';
-
 import { type FC } from 'react';
 
 import { Box } from '@/shared/ui';
+import { TOOLTIP_POSITION, type TooltipProps } from '@/shared/ui/tooltip';
 
 import styles from './Tooltip.module.scss';
+
+const { TOP } = TOOLTIP_POSITION;
 
 export const Tooltip: FC<TooltipProps> = ({
   children,
   message,
-  position = 'top',
+  position = TOP,
 }) => {
   return (
     <Box className={styles.container}>

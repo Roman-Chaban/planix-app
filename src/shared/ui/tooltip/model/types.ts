31da81bf76928f5e82ab1560bdc0467db19ee3a1
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-25 16:49:29 +0300

    Refactor: update TooltipPosition type to use TOOLTIP_POSITION for improved consistency

--- src/shared/ui/tooltip/model/types.ts
+++ src/shared/ui/tooltip/model/types.ts
@@ -1,9 +1,12 @@
 import { type ReactNode } from 'react';
 
-export type TooltipPosition = 'left' | 'right' | 'top' | 'bottom';
+import type { TOOLTIP_POSITION } from '@/shared/ui/tooltip';
 
 export type TooltipProps = {
   message: string;
   position?: TooltipPosition;
   children: ReactNode;
 };
+
+export type TooltipPosition =
+  (typeof TOOLTIP_POSITION)[keyof typeof TOOLTIP_POSITION];

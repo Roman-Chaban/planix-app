5a2c3fdc89d74725447700f358dc02d584235233
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-25 20:07:15 +0300

    Refactor: update useProfile to utilize getSession for user retrieval

--- src/entities/profile/api/model/useProfile.ts
+++ src/entities/profile/api/model/useProfile.ts
@@ -8,14 +8,18 @@ import { supabase } from '@/shared/api/supabase';
 export const useProfile = () => {
   return useQuery({
     queryKey: queryKeys.profile.all,
+
     queryFn: async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
+      const { data: session } = await supabase.auth.getSession();
+      const user = session.session?.user;
+
       if (!user) return null;
 
       return getProfile(user.id);
     },
+
     staleTime: 1000 * 60 * 5,
+
+    retry: false,
   });
 };

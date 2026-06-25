3b803b7d7335b71f01e3e01130547f2c84192596
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-25 23:59:16 +0300

    Feat: wrap ProfileLayout in Suspense for improved loading state handling

--- src/app/[locale]/settings/page.tsx
+++ src/app/[locale]/settings/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from 'react';
+
 import { ProfileLayout } from '@/widgets/profile';
+import { Loader } from '@/shared/ui';
 
 export default function ProfilePage() {
-  return <ProfileLayout />;
+  return (
+    <Suspense fallback={<Loader />}>
+      <ProfileLayout />
+    </Suspense>
+  );
 }

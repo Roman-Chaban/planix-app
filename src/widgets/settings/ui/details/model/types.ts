28ec1fdc565d37538421a96713b85e9f09549267
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-27 00:01:53 +0300

    Refactor: remove unnecessary blank line in types definition

--- src/widgets/settings/ui/details/model/types.ts
+++ src/widgets/settings/ui/details/model/types.ts
@@ -2,7 +2,6 @@ import type { TFunction } from 'i18next';
 
 import type { Profile } from '@/entities/settings';
 
-
 type ProfileProps = { profile: Profile; t: TFunction };
 
 export type ProfileInfoItemProps = {

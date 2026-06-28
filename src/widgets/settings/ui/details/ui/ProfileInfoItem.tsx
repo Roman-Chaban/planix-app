a358aa458032f80313a2937596b7f119cbe54cee
Roman Chaban <roman.chaban1001@gmail.com>
2026-06-28 18:47:08 +0300

    Refactor: delete unnecessary displayValue condition

--- src/widgets/settings/ui/details/ui/ProfileInfoItem.tsx
+++ src/widgets/settings/ui/details/ui/ProfileInfoItem.tsx
@@ -2,24 +2,18 @@ import type { ProfileInfoItemProps } from '../model/types';
 
 import type { FC } from 'react';
 
-import { toDisplay } from '@/shared/lib';
 import { Box, Typography } from '@/shared/ui';
 
 import styles from './ProfileDetails.module.scss';
 
 export const ProfileInfoItem: FC<ProfileInfoItemProps> = ({ label, value }) => {
-  const displayValue =
-    value instanceof Date
-      ? toDisplay(value.toISOString())
-      : toDisplay(String(value ?? ''));
-
   return (
     <Box className={styles.field}>
       <Typography as="span" className={styles.label}>
         {label}
       </Typography>
       <Typography as="span" className={styles.value}>
-        {displayValue}
+        {value}
       </Typography>
     </Box>
   );

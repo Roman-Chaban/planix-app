3cb55d93f95640fd19a5d4e221dfd378c9b2636a
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-01 20:15:03 +0300

    Refactor: implement useAuthStepper hook for improved step management and simplify AuthStepper component

--- src/features/auth/stepper/ui/AuthStepper.tsx
+++ src/features/auth/stepper/ui/AuthStepper.tsx
@@ -1,43 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
-
-import { useTranslation } from 'react-i18next';
-
-import {
-  AUTH_STEPS,
-  type AuthStep,
-  getAuthSteps,
-} from '@/features/auth/stepper';
-
-import { useLocalStorage } from '@/shared/lib/hooks';
-import { NAMESPACE as NS } from '@/shared/i18n';
-
 import { AuthWrapper } from '@/shared/ui';
 
-const { LOGIN } = AUTH_STEPS;
+import { useAuthStepper } from '../model/useAuthStepper';
 
 export const AuthStepper = () => {
-  const [step, setStep, hydrated] = useLocalStorage<AuthStep>(
-    'auth-step',
-    LOGIN,
-  );
-
-  const { t } = useTranslation(NS.AUTH);
-
-  const allSteps = getAuthSteps(t, setStep);
-
-  const currentStep = allSteps[step];
-
-  const { component: Component, header, footer } = currentStep;
-
-  useEffect(() => {
-    const hash = window.location.hash;
-
-    if (hash.includes('type=recovery')) {
-      setStep(AUTH_STEPS.RESET);
-    }
-  }, [setStep]);
+  const { header, footer, Component, hydrated, currentStep } = useAuthStepper();
 
   if (!currentStep) return null;
   if (!hydrated) return null;

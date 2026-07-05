8b2716c376723606fe9a714b831cbc9f1ef25d37
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-05 21:25:06 +0300

    Feat: add supabase mock for the tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+jest.mock('@/shared/api/supabase', () => ({
+  supabase: {
+    auth: {
+      signInWithPassword: jest.fn(),
+      onAuthStateChange: jest.fn(() => ({
+        data: { subscription: { unsubscribe: jest.fn() } },
+      })),
+    },
+  },
+}));

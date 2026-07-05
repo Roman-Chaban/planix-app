6bd3e9b4bd88caca773cf90554ccfe9c14f000a8
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-05 23:27:10 +0300

    Feat: add AuthProvider to GlobadProviders

--- src/app/providers/core/AppProviders.tsx
+++ src/app/providers/core/AppProviders.tsx
@@ -1,7 +1,10 @@
-import { I18nProvider } from '@/app/providers/i18n';
-import { QueryProvider } from '@/app/providers/query';
 import { SidebarProvider } from '@/features/sidebar';
+
 import type { AppLocale, WithChildren } from '@/shared/types/types';
+
+import { AuthProvider } from '../auth';
+import { I18nProvider } from '../i18n';
+import { QueryProvider } from '../query';
 
 type AppProvidersProps = WithChildren & {
   locale: AppLocale;
@@ -10,9 +13,11 @@ type AppProvidersProps = WithChildren & {
 export const AppProviders = ({ children, locale }: AppProvidersProps) => {
   return (
     <QueryProvider>
-      <SidebarProvider>
-        <I18nProvider locale={locale}>{children}</I18nProvider>
-      </SidebarProvider>
+      <AuthProvider>
+        <SidebarProvider>
+          <I18nProvider locale={locale}>{children}</I18nProvider>
+        </SidebarProvider>
+      </AuthProvider>
     </QueryProvider>
   );
 };

5e7575b831499f2c8981b01cfbd4b1c3e0744905
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-05 16:53:26 +0300

    Refactor: delete useless useQueryWithSkeleton hook, delete its types

--- src/shared/lib/hooks/model/types.ts
+++ src/shared/lib/hooks/model/types.ts
@@ -2,20 +2,6 @@ import type { zodResolver } from '@hookform/resolvers/zod';
 
 import type { FieldValues, UseFormProps } from 'react-hook-form';
 
-import {
-  type UseQueryResult,
-  type UseQueryOptions,
-} from '@tanstack/react-query';
-
-export type UseQueryWithSkeletonOptions<T> = UseQueryOptions<T> & {
-  Skeleton?: React.ComponentType;
-};
-
-export type UseQueryWithSkeletonResult<T> = UseQueryResult<T> & {
-  Skeleton?: React.ComponentType;
-  showSkeleton: boolean;
-};
-
 export type Point = { x: number; y: number };
 export type ScrollPosition = { left: number; top: number };
 export type Axis = 'x' | 'y' | 'both';

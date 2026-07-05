d8eec2575272faec951fc1961f58eee923535530
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-06 00:29:08 +0300

    Feat: add radiuses to skeleton

--- src/widgets/settings/profile-details/ui/skeleton/ProfileDetailsSkeleton.tsx
+++ src/widgets/settings/profile-details/ui/skeleton/ProfileDetailsSkeleton.tsx
@@ -22,19 +22,19 @@ export const ProfileDetailsSkeleton = () => {
               </Box>
 
               <Box className={styles.info}>
-                <Skeleton width={180} height={30} />
+                <Skeleton width={180} height={30} radius="20" />
 
-                <Skeleton width={240} height={27} />
+                <Skeleton width={240} height={27} radius="20" />
               </Box>
             </Box>
 
-            <Skeleton width={90} height={36} />
+            <Skeleton width={90} height={36} radius="20" />
           </Box>
         </Box>
         <Box className={styles.information}>
           {rows.map((_, rowIndex) => (
             <ProfileInfoItem key={rowIndex}>
-              <Skeleton height={30} />
+              <Skeleton height={30} radius="20" />
             </ProfileInfoItem>
           ))}
         </Box>

6d180973a40711389b05dc163201788c2dffb63c
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-06 01:24:01 +0300

    Feat: add SVGComponent type to global types

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,6 +1,6 @@
 import type { TFunction } from 'i18next';
 
-import type { ReactNode } from 'react';
+import type { FC, ReactNode, SVGProps } from 'react';
 
 import type { AuthStep } from '@/features/auth/stepper';
 import type { Locale } from '@/shared/i18n/locales/locales';
@@ -8,6 +8,7 @@ import type { Locale } from '@/shared/i18n/locales/locales';
 export type TranslateFn = TFunction;
 export type AppLocale = Locale;
 export type NavigateFn = (step: AuthStep) => void;
+export type SVGComponent = FC<SVGProps<SVGSVGElement>>;
 
 export type WithChildren = {
   children?: ReactNode;

7ec622e33312422afab37d9d2183e6233d4377da
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-05 23:34:18 +0300

    Refactor: rewrite profile queryKeys, make it byId

--- src/entities/settings/api/queryKeys/queryKeys.ts
+++ src/entities/settings/api/queryKeys/queryKeys.ts
@@ -1,6 +1,5 @@
 export const queryKeys = {
   profile: {
-    all: ['profile'] as const,
-    details: (id: string) => ['profile', id] as const,
+    byId: (id?: string) => ['profile', id] as const,
   },
 };

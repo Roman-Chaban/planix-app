a0416b6c7df98fb01a32db0edd03d944bfa97944
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-07 00:39:49 +0300

    Feat: add drag feature into Billing

--- src/widgets/settings/subscription-billing/billing/ui/Billing.tsx
+++ src/widgets/settings/subscription-billing/billing/ui/Billing.tsx
@@ -6,11 +6,14 @@ import { useTranslation } from 'react-i18next';
 
 import { useBillingHistory } from '@/entities/billing';
 import { NAMESPACE as NS } from '@/shared/i18n';
+import { AXIS } from '@/shared/lib/hooks';
 import { Box, DataTable, SettingsHeader } from '@/shared/ui';
 
 import { getBillingHistoryColumns } from '../lib/get-billing-history-columns';
 
 import styles from './Billing.module.scss';
+
+const { X } = AXIS;
 
 export const Billing = ({ skeleton: Skeleton }: BillingProps) => {
   const { t } = useTranslation(NS.SETTINGS);
@@ -36,6 +39,7 @@ export const Billing = ({ skeleton: Skeleton }: BillingProps) => {
           getRowKey={(key) => key.id}
           size="sm"
           variant="default"
+          dragAxis={X}
         />
       )}
     </Box>

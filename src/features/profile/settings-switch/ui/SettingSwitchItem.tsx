13634ed8ed23d0ada55cf6e7fc06a87536a58c26
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-08 01:21:51 +0300

    Feat: add SettingSwitch components, styles and index

---
+++ src/features/profile/settings-switch/ui/SettingSwitchItem.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useTranslation } from 'react-i18next';
+
+import { NAMESPACE as NS } from '@/shared/i18n';
+import { useLocalStorage } from '@/shared/lib/hooks';
+import type { WithSwitch } from '@/shared/types/types';
+import { Box, Switch, Typography } from '@/shared/ui';
+
+import styles from './SettingSwitch.module.scss';
+
+export const SettingSwitchItem = ({ id, icon: Icon, title, subtitle }: WithSwitch) => {
+  const { t } = useTranslation(NS.SETTINGS);
+  const [isOn, setIsOn, hydrated] = useLocalStorage<boolean>(`switch-${id}`, false);
+
+  if (!hydrated) {
+    return null;
+  }
+
+  return (
+    <Box className={styles.switch}>
+      <Box className={styles.switchContainer}>
+        <Box className={styles.switchIcon}>{Icon ? <Icon /> : null}</Box>
+        <Box className={styles.switchInfo}>
+          <Typography as="h3" className={styles.switchTitle}>
+            {t(title)}
+          </Typography>
+          <Typography as="p" className={styles.switchSubtitle}>
+            {t(subtitle)}
+          </Typography>
+        </Box>
+      </Box>
+      <Switch isOn={isOn} onToggle={setIsOn} />
+    </Box>
+  );
+};
---
+++ src/features/profile/settings-switch/ui/SettingSwitchList.tsx
@@ -0,0 +1,20 @@
+import type { IdentifiableSwitch, SettingSwitchListProps } from '@/shared/types/types';
+import { List } from '@/shared/ui';
+
+import styles from './SettingSwitch.module.scss';
+import { SettingSwitchItem } from './SettingSwitchItem';
+
+export const SettingSwitchList = <T extends IdentifiableSwitch>({
+  itemClassName,
+  list,
+}: SettingSwitchListProps<T>) => {
+  return (
+    <List
+      className={styles.switchList}
+      itemClassName={itemClassName}
+      renderList={list}
+      getItemKey={({ id }) => id}
+      renderItem={(item) => <SettingSwitchItem {...item} />}
+    />
+  );
+};

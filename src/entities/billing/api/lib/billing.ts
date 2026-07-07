f66ce4b63b2b1844f71fe8ec2ecf608c45b938b0
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-07 19:50:29 +0300

    Refactor: change hardcode literals to constants

--- src/entities/billing/api/lib/billing.ts
+++ src/entities/billing/api/lib/billing.ts
@@ -1,15 +1,16 @@
 import type { Billing } from '../model/types';
 
+import { BILLING_HISTORY, PAYMENT_DATE, USER_ID } from '@/shared/api';
 import { supabase } from '@/shared/api/supabase';
 
 import { mapBilling } from './billing.mapper';
 
 export const getBillingHistory = async (userId: string): Promise<Billing[]> => {
   const { data, error } = await supabase
-    .from('billing_history')
+    .from(BILLING_HISTORY)
     .select('*')
-    .eq('user_id', userId)
-    .order('payment_date', { ascending: false });
+    .eq(USER_ID, userId)
+    .order(PAYMENT_DATE, { ascending: false });
 
   if (error) {
     throw error;

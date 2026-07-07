454864346f7db5ceda79c8ea193244d2787a3d3b
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-07 20:19:30 +0300

    Feat: add constants for supabase, update it in index

---
+++ src/shared/api/index.ts
@@ -0,0 +1,9 @@
+export { supabase } from './supabase';
+
+export {
+  AVATARS,
+  PROFILES,
+  BILLING_HISTORY,
+  USER_ID,
+  PAYMENT_DATE,
+} from './lib/supabase.constants';
---
+++ src/shared/api/lib/supabase.constants.ts
@@ -0,0 +1,5 @@
+export const AVATARS = 'avatars';
+export const PROFILES = 'profiles';
+export const BILLING_HISTORY = 'billing_history';
+export const USER_ID = 'user_id';
+export const PAYMENT_DATE = 'payment_date';

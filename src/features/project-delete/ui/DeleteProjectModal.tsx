7403b6362e8e85654ecbee0bd124b0ddc4f39063
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-09 01:07:02 +0300

    Refactor: add ModalActions, change button props to presets, update styles for modal content

--- src/features/project-delete/ui/DeleteProjectModal.tsx
+++ src/features/project-delete/ui/DeleteProjectModal.tsx
@@ -4,19 +4,10 @@ import { useTranslation } from 'react-i18next';
 
 import { useDeleteProjectModal, type DeleteProjectModalProps } from '@/features/project-delete';
 import { NAMESPACE as NS } from '@/shared/i18n/namespaces/namespaces';
-import { Modal, ModalHeader, Textarea, Box, Button } from '@/shared/ui';
-import {
-  BUTTON_SIZES,
-  BUTTON_TYPES,
-  BUTTON_VARIANTS,
-} from '@/shared/ui/button/lib/button.constants';
+import { Modal, ModalHeader, Textarea, Button, ModalContent, ModalActions } from '@/shared/ui';
 import { CloseIcon } from '@/shared/ui/icons';
 
 import styles from './DeleteProjectModal.module.scss';
-
-const { BUTTON } = BUTTON_TYPES;
-const { OUTLINE, DEFAULT } = BUTTON_VARIANTS;
-const { LARGE } = BUTTON_SIZES;
 
 export const DeleteProjectModal = ({ projectId, onClose, isOpen }: DeleteProjectModalProps) => {
   const { t } = useTranslation(NS.MODAL);
@@ -29,42 +20,35 @@ export const DeleteProjectModal = ({ projectId, onClose, isOpen }: DeleteProject
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
-      <ModalHeader onClose={handleClose} title={t('title')} icon={<CloseIcon />} />
+      <ModalContent className={styles.content}>
+        <ModalHeader onClose={handleClose} title={t('title')} icon={<CloseIcon />} />
 
-      <Textarea
-        value={reason}
-        onChange={(event) => setReason(event.target.value)}
-        placeholder={t('placeholder')}
-        label={t('label')}
-        textareaClassName={styles.textarea}
-        labelClassName={styles.reasonLabel}
-        disabled={isProjectActionPending}
-      />
+        <Textarea
+          value={reason}
+          onChange={(event) => setReason(event.target.value)}
+          placeholder={t('placeholder')}
+          label={t('label')}
+          textareaClassName={styles.textarea}
+          labelClassName={styles.reasonLabel}
+          disabled={isProjectActionPending}
+        />
 
-      <Box className={styles.actions}>
-        <Button
-          fullWidth
-          type={BUTTON}
-          variant={OUTLINE}
-          size={LARGE}
-          className={styles.cancel}
-          onClick={onClose}
-        >
-          {t('cancelButton')}
-        </Button>
+        <ModalActions>
+          <Button preset="MODAL_OUTLINE" minWidth="lg" className={styles.cancel} onClick={onClose}>
+            {t('cancelButton')}
+          </Button>
 
-        <Button
-          fullWidth
-          type={BUTTON}
-          variant={DEFAULT}
-          size={LARGE}
-          disabled={!isDeleted}
-          className={styles.save}
-          onClick={handleDeleteProject}
-        >
-          {t('saveButton')}
-        </Button>
-      </Box>
+          <Button
+            preset="MODAL_DEFAULT"
+            minWidth="lg"
+            disabled={!isDeleted}
+            className={styles.save}
+            onClick={handleDeleteProject}
+          >
+            {t('saveButton')}
+          </Button>
+        </ModalActions>
+      </ModalContent>
     </Modal>
   );
 };

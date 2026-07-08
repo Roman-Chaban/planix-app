502ccacf4f276e0ae49b803e5838b121c8928dbb
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-08 20:12:44 +0300

    Feat: delete useless SystemSecurity component, move its business logic to System widget

--- src/features/profile/system-security/index.ts
+++
@@ -1,1 +0,0 @@
-export { SystemSecurity } from './ui/SystemSecurity';
--- src/features/profile/system-security/ui/SystemSecurity.tsx
+++
@@ -1,6 +0,0 @@
-import { SettingSwitchList } from '../../settings-switch/ui/SettingSwitchList';
-import { securityList } from '../lib/security-list';
-
-export const SystemSecurity = () => {
-  return <SettingSwitchList list={securityList} />;
-};

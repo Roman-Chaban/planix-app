b86f9aebb86a022abc00966d07e01faf091a78ab
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-10 22:49:23 +0300

    Feat: add useLogoutAction hook

---
+++ src/features/profile/logout/model/useLogoutAction.ts
@@ -0,0 +1,53 @@
+import { useTranslation } from 'react-i18next';
+
+import { useAppDispatch } from '@/app/providers/store/hooks';
+import { ROUTES } from '@/app/routes';
+import { closeModal } from '@/entities/modal';
+import { showToast } from '@/entities/toast';
+import { NAMESPACE as NS } from '@/shared/i18n';
+import { useLocalizedRouter } from '@/shared/lib/hooks';
+
+import { TOAST_VARIANT } from '@/shared/ui/toast';
+
+import { useLogout } from './useLogout';
+
+const { DASHBOARD } = ROUTES;
+const { SUCCESS, ERROR } = TOAST_VARIANT;
+
+export const useLogoutAction = () => {
+  const { t } = useTranslation(NS.LOGOUT_MODAL);
+
+  const localizedRouter = useLocalizedRouter();
+  const dispatch = useAppDispatch();
+
+  const { logout, isLoading } = useLogout();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+
+      dispatch(closeModal());
+
+      dispatch(
+        showToast({
+          variant: SUCCESS,
+          description: t('toast.logoutSuccess'),
+        }),
+      );
+
+      localizedRouter.replace(DASHBOARD);
+    } catch {
+      dispatch(
+        showToast({
+          variant: ERROR,
+          description: t('toast.logoutError'),
+        }),
+      );
+    }
+  };
+
+  return {
+    handleLogout,
+    isLoading,
+  };
+};

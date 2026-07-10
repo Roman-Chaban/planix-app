9438f4205dc95359e772fcc0febe491b257eb008
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-10 23:27:09 +0300

    Feat: add SystemSection and provide it into System component

--- src/widgets/settings/system/ui/System.tsx
+++ src/widgets/settings/system/ui/System.tsx
@@ -6,36 +6,36 @@ import { LanguageSelect } from '@/features/language-switcher';
 import { ChangePasswordForm } from '@/features/profile/change-password';
 import { SettingSwitchList } from '@/features/profile/settings-switch';
 import { NAMESPACE as NS } from '@/shared/i18n';
-import { Box, SettingsHeader } from '@/shared/ui';
+import { Box } from '@/shared/ui';
 
 import { securityList } from '../lib/security-list';
 
 import styles from './System.module.scss';
+import { SystemSection } from './SystemSection';
 
 export const System = () => {
   const { t } = useTranslation(NS.SETTINGS);
 
   return (
     <Box className={styles.system}>
-      <Box className={styles.systemContainer}>
-        <SettingsHeader title={t('system.languageTitle')} />
+      <SystemSection title={t('system.languageTitle')}>
         <Box className={styles.content}>
           <LanguageSelect />
         </Box>
-      </Box>
-      <Box className={styles.systemContainer}>
-        <SettingsHeader title={t('system.securityTitle')} />
-        <SettingSwitchList list={securityList} t={t} />
-      </Box>
-      <Box className={styles.systemContainer}>
-        <SettingsHeader title={t('system.changePasswordTitle')} />
+      </SystemSection>
+
+      <SystemSection title={t('system.securityTitle')}>
+        <SettingSwitchList list={securityList} />
+      </SystemSection>
+
+      <SystemSection title={t('system.changePasswordTitle')}>
         <Box className={styles.content}>
           <ChangePasswordForm
             translationNamespace={NS.SETTINGS}
             buttonLabel={t('system.savePassword')}
           />
         </Box>
-      </Box>
+      </SystemSection>
     </Box>
   );
 };
---
+++ src/widgets/settings/system/ui/SystemSection.tsx
@@ -0,0 +1,14 @@
+import type { SystemSectionProps } from '../model/system.types';
+
+import { Box, SettingsHeader } from '@/shared/ui';
+
+import styles from './System.module.scss';
+
+export const SystemSection = ({ title, children }: SystemSectionProps) => {
+  return (
+    <Box className={styles.systemContainer}>
+      <SettingsHeader title={title} />
+      {children}
+    </Box>
+  );
+};

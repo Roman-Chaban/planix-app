c2127a7bb09fdcf945a2b0f0635f4bf2e44bffb8
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-10 22:46:03 +0300

    Refactor: change type in provider, update devToolts in store, add modal and toast slices,  update index

--- src/app/providers/store/rootReducer.ts
+++ src/app/providers/store/rootReducer.ts
@@ -1,3 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-export const rootReducer = combineReducers({});
+import { modalReducer } from '@/entities/modal';
+import { toastReducer } from '@/entities/toast';
+
+export const rootReducer = combineReducers({
+  modal: modalReducer,
+  toast: toastReducer,
+});

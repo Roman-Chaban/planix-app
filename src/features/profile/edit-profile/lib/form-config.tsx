f55f24536c22009cdad31e3fcb7242c7733370f2
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-13 18:29:45 +0300

    Feat: add icons for editFields

--- src/features/profile/edit-profile/lib/form-config.tsx
+++ src/features/profile/edit-profile/lib/form-config.tsx
@@ -1,6 +1,7 @@
 import type { ProfileFormValues } from '../model/edit-profile.types';
 
 import type { FormFieldConfig } from '@/shared/ui/form/form-fields';
+import { CalendarIcon, ClientIcon, ContactIcon, MessageIcon } from '@/shared/ui/icons';
 import { INPUT_TYPES } from '@/shared/ui/input';
 
 const { TEXT, EMAIL } = INPUT_TYPES;
@@ -13,29 +14,34 @@ export const editFields: readonly FormFieldConfig<ProfileFormValues>[] = [
     label: 'profileDetails.firstName',
     type: TEXT,
     className: styles.firstName,
+    startIcon: <ClientIcon />,
   },
   {
     name: 'lastName',
     label: 'profileDetails.lastName',
     type: TEXT,
     className: styles.lastName,
+    startIcon: <ClientIcon />,
   },
   {
     name: 'email',
     label: 'profileDetails.email',
     type: EMAIL,
     className: styles.email,
+    startIcon: <MessageIcon />
   },
   {
     name: 'birthDate',
     label: 'profileDetails.birthDate',
     type: TEXT,
     className: styles.birthDate,
+    startIcon: <CalendarIcon />
   },
   {
     name: 'contact',
     label: 'profileDetails.contact',
     type: TEXT,
     className: styles.contact,
+    startIcon: <ContactIcon />
   },
 ] as const;

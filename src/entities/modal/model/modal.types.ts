bdfef76b21b4f035fb258f90a07a4b5d5d967c33
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-16 20:15:55 +0300

    Feat: add containerId as a prop, implement this prop for Portal in Modal

--- src/entities/modal/model/modal.types.ts
+++ src/entities/modal/model/modal.types.ts
@@ -1,5 +1,6 @@
 export enum ModalId {
   LOGOUT = 'logout',
+  VERIFY_EMAIL = 'verifyEmail',
 }
 
 export type ModalState = {
--- src/shared/ui/modal/ui/Modal.tsx
+++ src/shared/ui/modal/ui/Modal.tsx
@@ -3,13 +3,19 @@
 import { ModalOverlay, Portal } from '@/shared/ui';
 import { useModalBehavior, type ModalProps } from '@/shared/ui/modal';
 
-export const Modal = ({ isOpen, onClose, closeOnOverlayClick = true, children }: ModalProps) => {
+export const Modal = ({
+  isOpen,
+  onClose,
+  closeOnOverlayClick = true,
+  children,
+  containerId = 'modal-root',
+}: ModalProps) => {
   useModalBehavior({ isOpen, onClose });
 
   if (!isOpen) return null;
 
   return (
-    <Portal containerId="modal-root">
+    <Portal containerId={containerId}>
       <ModalOverlay onClose={onClose} closeOnOverlayClick={closeOnOverlayClick}>
         {children}
       </ModalOverlay>

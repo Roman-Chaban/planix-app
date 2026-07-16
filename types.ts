d06a88a5e4ca169f4f032ab31dff9b8143c42283
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-16 20:15:18 +0300

    Feat: add EmailSentFn global type

--- types.ts
+++ types.ts
@@ -8,6 +8,7 @@ import type { Locale } from '@/shared/i18n/locales/locales';
 export type TranslateFn = TFunction;
 export type AppLocale = Locale;
 export type NavigateFn = (step: AuthStep) => void;
+export type EmailSentFn = (email: string) => void;
 export type SVGComponent = FC<SVGProps<SVGSVGElement>>;
 
 export type WithChildren = {

075544249e13931963c64a6d8602331b7aa1c9ab
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-19 18:19:49 +0300

    Refactor: add AuthStep integration, make header and footer with functions, add ForgotModal

--- src/features/auth/stepper/ui/AuthStepper.tsx
+++ src/features/auth/stepper/ui/AuthStepper.tsx
@@ -2,14 +2,32 @@
 
 import { AuthWrapper } from '@/shared/ui';
 
+import { ForgotModal } from '../../recover-password/forgot/ui/ForgotModal';
+import { getAuthFooter } from '../lib/get-auth-footer';
+import { getAuthHeader } from '../lib/get-auth-header';
 import { useAuthStepper } from '../model/useAuthStepper';
 
+import { AuthStep } from './AuthStep';
+
 export const AuthStepper = () => {
-  const { header, footer, Component } = useAuthStepper();
+  const { activeStep, resetEmail, navigateToStep, handleEmailSent, t } = useAuthStepper();
+
+  const header = getAuthHeader({
+    activeStep,
+    t,
+  });
+
+  const footer = getAuthFooter({
+    activeStep,
+    navigateToStep,
+    t,
+  });
 
   return (
     <AuthWrapper header={header} footer={footer}>
-      <Component />
+      <AuthStep step={activeStep} onNavigate={navigateToStep} onEmailSent={handleEmailSent} />
+
+      <ForgotModal email={resetEmail} />
     </AuthWrapper>
   );
 };

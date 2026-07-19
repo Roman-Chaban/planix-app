1f3f6c0a816877472b522cb45a90f5c309c8d327
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-19 18:26:45 +0300

    Feat: update index with exports

--- src/features/auth/stepper/index.ts
+++ src/features/auth/stepper/index.ts
@@ -1,5 +1,10 @@
 export { AUTH_STEPS, type AuthStep } from './lib/stepper.constants';
 
+export { getAuthFooter } from './lib/get-auth-footer';
+export { getAuthHeader } from './lib/get-auth-header';
+
+export type { AuthFooterProps, AuthHeaderProps, AuthStepProps } from './model/stepper.types';
+
 export { useAuthStepper } from './model/useAuthStepper';
 
-export { getAuthSteps } from './lib/auth-steps.config';
+export { AuthStepper } from './ui/AuthStepper';

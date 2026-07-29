a336d41bdac0fa5836bd288c86eb7e15acda26ac
Roman Chaban <roman.chaban1001@gmail.com>
2026-07-29 20:02:44 +0300

    Feat: change Loader to PageLoader for SettingsPage

--- src/app/[locale]/settings/page.tsx
+++ src/app/[locale]/settings/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react';
 
 import { SettingsLayout } from '@/widgets/settings';
-import { Loader } from '@/shared/ui';
+import { PageLoader } from '@/shared/ui';
 
 export default function SettingsPage() {
   return (
-    <Suspense fallback={<Loader />}>
+    <Suspense fallback={<PageLoader />}>
       <SettingsLayout />
     </Suspense>
   );

3984462adb7cab2694bbab14d74d0774fb2b47c9
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-03 19:20:09 +0300

    Feat: add subscription plans entity with hooks and query keys

---
+++ src/entities/susbcription-plans/index.ts
@@ -0,0 +1,13 @@
+export { mapSubscriptionPlan } from './lib/subscription-plans.mapper';
+export { getSubscriptionPlans } from './lib/susbcription-plans';
+
+export type {
+  Plan,
+  SubscriptionPlanMapOptions,
+  UseSubscriptionPlansOptions,
+  SubscriptionPlanCard,
+} from './model/susbcription-plans.types';
+
+export { useSubscriptionPlans } from './model/useSubscriptionPlans';
+
+export { queryKeys } from './queryKeys/queryKeys';

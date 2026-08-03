e8664ba9f6907d01a1a9b2d3207aec40174e5fa1
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-03 19:22:04 +0300

    Feat: replace CheckIcon with PlanCheckIcon in Checkbox component

--- src/shared/ui/checkbox/ui/Checkbox.tsx
+++ src/shared/ui/checkbox/ui/Checkbox.tsx
@@ -4,7 +4,7 @@ import { buildClassName } from '@/shared/lib';
 
 import { Typography } from '@/shared/ui';
 import type { CheckboxProps } from '@/shared/ui/checkbox';
-import { CheckIcon } from '@/shared/ui/icons/Icons';
+import { PlanCheckIcon } from '@/shared/ui/icons/Icons';
 import { INPUT_TYPES } from '@/shared/ui/input';
 
 import styles from './Checkbox.module.scss';
@@ -33,7 +33,7 @@ export const Checkbox = ({ checked, onChange, label, disabled }: CheckboxProps) 
       />
 
       <Typography as="span" className={styles.box}>
-        <CheckIcon className={buildClassName(styles.checkIcon, checked && styles.visible)} />
+        <PlanCheckIcon className={buildClassName(styles.checkIcon, checked && styles.visible)} />
       </Typography>
 
       {label && (

027ed119e69ec07f90c0f88f32fc23db518d6fd5
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-03 22:02:50 +0300

    Feat: enhance SubscriptionPlans component with loading state and skeleton rendering

--- src/widgets/subscription/ui/plans/ui/SubscriptionPlans.tsx
+++ src/widgets/subscription/ui/plans/ui/SubscriptionPlans.tsx
@@ -7,18 +7,33 @@ import { useSubscriptionPlans } from '@/entities/susbcription-plans';
 import { NAMESPACE as NS } from '@/shared/i18n';
 import { List } from '@/shared/ui';
 
+import { SubscriptionPlanSkeleton } from '../../plan';
 import { SubscriptionPlan } from '../../plan/ui/SubscriptionPlan';
+
+import { SKELETONS } from '../lib/plans.constants';
 
 import styles from './SubscriptionPlans.module.scss';
 
 export const SubscriptionPlans = () => {
   const { i18n, t } = useTranslation(NS.SUBSCRIPTION);
 
-  const { plans } = useSubscriptionPlans({
+  const { plans, isLoading } = useSubscriptionPlans({
     lng: i18n.language,
   });
 
-  const [selectedPlan, setSelectedPlan] = useState(plans[0]?.id);
+  const [selectedPlan, setSelectedPlan] = useState<string | null>(null);
+  const activePlanId = selectedPlan ?? plans[1]?.id ?? plans[0]?.id;
+
+  if (isLoading) {
+    return (
+      <List
+        className={styles.subscriptionPlans}
+        renderList={SKELETONS}
+        getItemKey={(item) => item}
+        renderItem={() => <SubscriptionPlanSkeleton />}
+      />
+    );
+  }
 
   return (
     <List
@@ -27,7 +42,7 @@ export const SubscriptionPlans = () => {
       getItemKey={(plan) => plan.id}
       renderItem={(plan) => (
         <SubscriptionPlan
-          isActive={selectedPlan === plan.id}
+          isActive={activePlanId === plan.id}
           onActive={() => setSelectedPlan(plan.id)}
           plan={plan}
           monthLabel={t('planCard.month')}

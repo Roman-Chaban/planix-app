2d4d4d2d765c198711a34a430e83a9e86a82457e
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-04 22:30:18 +0300

    Feat: enhance avatar upload functionality with toast notifications

--- src/widgets/settings/details/model/useUploadAvatar.ts
+++ src/widgets/settings/details/model/useUploadAvatar.ts
@@ -1,8 +1,20 @@
 import { type ChangeEvent, useRef } from 'react';
 
+import { useTranslation } from 'react-i18next';
+
+import { useAppDispatch } from '@/app/providers/store/hooks';
 import { uploadProfileAvatar } from '@/entities/profile';
+import { showToast } from '@/entities/toast';
+import { NAMESPACE as NS } from '@/shared/i18n';
+import { TOAST_VARIANT } from '@/shared/ui/toast';
+
+const { SUCCESS, ERROR } = TOAST_VARIANT;
 
 export const useUploadAvatar = () => {
+  const dispatch = useAppDispatch();
+
+  const { t } = useTranslation(NS.SETTINGS);
+
   const uploadAvatarRef = useRef<HTMLInputElement>(null);
 
   const handleUploadClick = () => {
@@ -14,7 +26,25 @@ export const useUploadAvatar = () => {
 
     if (!file) return;
 
-    await uploadProfileAvatar(userId, file);
+    try {
+      await uploadProfileAvatar(userId, file);
+
+      dispatch(
+        showToast({
+          variant: SUCCESS,
+          description: t('profileDetails.toast.success'),
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        showToast({
+          variant: ERROR,
+          description: t('profileDetails.toast.error'),
+        }),
+      );
+
+      console.error(error);
+    }
 
     event.target.value = '';
   };

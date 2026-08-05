9a24cef5edc844b3adb2d91b399134df9249f019
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-05 20:15:07 +0300

    Refactor: update import paths for AppProviders to improve consistency

--- src/shared/providers/core/AppProviders.tsx
+++ src/shared/providers/core/AppProviders.tsx
@@ -1,12 +1,12 @@
-import type { AppProvidersProps } from '../../../app/providers/core/types';
+import type { AppProvidersProps } from './core.types';
 
 import { AppToast } from '@/entities/toast';
 
-import { I18nProvider } from '../../../app/providers/i18n';
-import { QueryProvider } from '../query';
 import { SidebarProvider } from '../../../app/providers/sidebar';
 import { StoreProvider } from '../../../app/providers/store';
 import { AuthProvider } from '../auth';
+import { I18nProvider } from '../i18n';
+import { QueryProvider } from '../query';
 
 export const AppProviders = ({ children, locale }: AppProvidersProps) => {
   return (

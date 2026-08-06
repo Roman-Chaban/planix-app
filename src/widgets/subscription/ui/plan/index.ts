35f15b0c01e202f778529e3b2af326286fe673c9
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-06 20:53:14 +0300

    Refactor: reorganize skeleton components for consistency

--- src/widgets/subscription/ui/plan/index.ts
+++ src/widgets/subscription/ui/plan/index.ts
@@ -1,4 +1,4 @@
 export { SubscriptionPlan } from './ui/SubscriptionPlan';
 export { SubscriptionPlanFeature } from './ui/SubscriptionPlanFeature';
 
-export { SubscriptionPlanSkeleton, SubscriptionPlanFeatureSkeleton } from './skeleton';
+export { SubscriptionPlanSkeleton, SubscriptionPlanFeatureSkeleton } from './ui/skeleton';

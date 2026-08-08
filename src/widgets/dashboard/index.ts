9c582fd8ba6773bb461054b370333d43b304fec4
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-08 18:23:13 +0300

    Refactor: reorganize dashboard components and styles for improved structure and maintainability

--- src/widgets/dashboard/index.ts
+++ src/widgets/dashboard/index.ts
@@ -1,3 +1,3 @@
-export { Analytics, AnalyticsHeader, AnalyticsHeaders, AnalyticsRevenue } from './analytics';
-export { Cards } from './cards';
-export { Dashboard } from './Dashboard';
+export { Analytics, AnalyticsHeader, AnalyticsHeaders, AnalyticsRevenue } from './ui/analytics';
+export { Cards } from './ui/cards';
+export { Dashboard } from './ui/Dashboard';

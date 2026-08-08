50c2e66cbfc4f5855fe46282de99f8538e0fcf25
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-08 18:24:03 +0300

    Refactor: create analytics and cards types for improved structure and type safety

--- src/widgets/dashboard/ui/analytics/ui/AnalyticsHeader.tsx
+++ src/widgets/dashboard/ui/analytics/ui/AnalyticsHeader.tsx
@@ -1,4 +1,4 @@
-import type { AnalyticsHeaderProps } from '../model/types';
+import type { AnalyticsHeaderProps } from '../model/analytics.types';
 
 import { Box, Typography } from '@/shared/ui';
 

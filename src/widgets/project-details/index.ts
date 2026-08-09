a46eba5ae57557b3b6a96be9bac335b9b9ed031f
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-09 20:42:10 +0300

    Feat: export getHeaderItems from header lib in Project Details widget

--- src/widgets/project-details/index.ts
+++ src/widgets/project-details/index.ts
@@ -1,3 +1,5 @@
+export { getHeaderItems } from './ui/header/lib/get-header-items';
+
 export { ProjectDetails } from './ui/ProjectDetails';
 export { Description } from './ui/description';
 export { FileItem } from './ui/file-item';

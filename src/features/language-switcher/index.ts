79080e9ea3d780025129eb51685723a187689f9b
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-09 20:52:41 +0300

    Feat: remove LanguageSelectStyles and related styles from language switcher

--- src/features/language-switcher/index.ts
+++ src/features/language-switcher/index.ts
@@ -1,12 +1,10 @@
 export { INSTANCE_ID } from './lib/language-switcher.constants';
 
-export type { LanguageOption, LanguageSelectStyles } from './model/language-switcher.types';
+export type { LanguageOption } from './model/language-switcher.types';
 
 export { languageOptions } from './lib/language-switcher-options';
 
 export { useLanguageSwitcher } from './model/useLanguageSwitcher';
-
-export { createSelectStyles } from './ui/styles';
 
 export { LanguageSwitcher } from './ui/LanguageSwitcher';
 export { Option } from './ui/Option';

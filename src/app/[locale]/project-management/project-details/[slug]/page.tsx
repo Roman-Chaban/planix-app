b570c9ca248ad955a3a18c2f3b1e9d487155164e
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-09 15:43:06 +0300

    Feat: refactor project details page to use constants for table and column names

--- src/app/[locale]/project-management/project-details/[slug]/page.tsx
+++ src/app/[locale]/project-management/project-details/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import { ProjectDetails } from '@/widgets/project-details/ui/ProjectDetails';
+import { PROJECTS, SLUG } from '@/shared/api';
 import { supabase } from '@/shared/api/supabase';
 
 export default async function ProjectPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
 
-  const { data: project } = await supabase.from('Projects').select('*').eq('slug', slug).single();
+  const { data: project } = await supabase.from(PROJECTS).select('*').eq(SLUG, slug).single();
 
   return <ProjectDetails project={project} />;
 }

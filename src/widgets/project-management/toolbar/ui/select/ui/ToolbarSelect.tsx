11bdf7360ae0b0ae7425de960fa8398e75bac648
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-10 23:01:44 +0300

    Feat: add ToolbarSelect styles and integrate with component

--- src/widgets/project-management/toolbar/ui/select/ui/ToolbarSelect.tsx
+++ src/widgets/project-management/toolbar/ui/select/ui/ToolbarSelect.tsx
@@ -2,15 +2,22 @@
 
 import type { ToolbarSelectProps, ToolbarSelectOption } from '../model/select.types';
 
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { getHeaderItems } from '@/widgets/project-details';
 import { NAMESPACE as NS } from '@/shared/i18n';
 
-import { Select } from '@/shared/ui';
+import { buildClassName } from '@/shared/lib';
+import { useSidebar } from '@/shared/providers/sidebar';
+import { createSelectStyles, Select } from '@/shared/ui';
+
+import styles from './ToolbarSelect.module.scss';
 
 export const ToolbarSelect = ({ activeId, setActiveId }: ToolbarSelectProps) => {
   const { t } = useTranslation(NS.PROJECT_MANAGEMENT);
+
+  const { isSidebarOpen } = useSidebar();
 
   const headerItems = getHeaderItems(t);
 
@@ -21,12 +28,17 @@ export const ToolbarSelect = ({ activeId, setActiveId }: ToolbarSelectProps) => 
 
   const selectedOption = options.find((option) => option.value === activeId) ?? null;
 
+  const selectStyles = useMemo(() => createSelectStyles<ToolbarSelectOption>(), []);
+
   return (
     <Select<ToolbarSelectOption, false>
+      className={buildClassName(styles.select, { [styles.selectCollapsed]: isSidebarOpen })}
       value={selectedOption}
       options={options}
+      styles={selectStyles}
       isSearchable={false}
       isClearable={false}
+
       onChange={(option) => {
         if (!option) {
           return;

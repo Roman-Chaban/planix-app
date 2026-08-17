cc038313f4ea6b9be6efc3c6f1bc5f2440b726ef
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-17 21:07:37 +0300

    Refactor: delete hardcode JSX, use TableSkeleton instead of that

--- src/widgets/settings/ui/billing/ui/skeleton/BillingSkeleton.tsx
+++ src/widgets/settings/ui/billing/ui/skeleton/BillingSkeleton.tsx
@@ -1,57 +1,20 @@
 import type { TranslateFn } from '@types';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-} from '@/shared/ui';
-import { Skeleton } from '@/shared/ui/skeleton/ui/Skeleton';
-
+import { TableSkeleton } from '@/shared/ui';
+import { TABLE_SKELETON_ROWS } from '@/shared/ui/skeletons/table';
 import type { TableSize } from '@/shared/ui/table/model/types';
 
 import { getBillingHistoryColumns } from '../../lib/get-billing-history-columns';
+
+const { SM } = TABLE_SKELETON_ROWS;
 
 type BillingSkeletonProps = {
   t: TranslateFn;
   size: TableSize;
 };
 
-const ROWS_COUNT = 5;
-
 export const BillingTableSkeleton = ({ t, size }: BillingSkeletonProps) => {
   const columns = getBillingHistoryColumns(t);
 
-  const rows = Array.from({ length: ROWS_COUNT });
-
-  return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableHeadCell size={size} key={column.key}>
-                {column.title}
-              </TableHeadCell>
-            ))}
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {rows.map((_, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {columns.map((column) => (
-                <TableCell key={column.key} size={size}>
-                  <Skeleton height={25} radius={30} />
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  return <TableSkeleton columns={columns} rowsCount={SM} size={size} />;
 };

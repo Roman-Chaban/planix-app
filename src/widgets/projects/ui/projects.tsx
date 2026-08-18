8de2e592cf577aa8a5228f94dc139ed0f9503c52
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-18 22:25:42 +0300

    Feat: add filteredProjects prop in hook, use it for ProjectsTable

--- src/widgets/projects/ui/projects.tsx
+++ src/widgets/projects/ui/projects.tsx
@@ -5,7 +5,7 @@ import { Header } from '@/widgets/header';
 import { DeleteProjectModal } from '@/features/project-delete';
 import { PageWrapper } from '@/shared/ui';
 
-import { useProjectsPageModel } from '../model/use-projects';
+import { useProjectsPageModel } from '../model/use-projects-page-model';
 
 import { EmptyState } from './empty-state/empty-state';
 import { ProjectsTable } from './projects-table/projects-table';
@@ -13,19 +13,27 @@ import styles from './projects.module.scss';
 import { Toolbar } from './toolbar/toolbar';
 
 export const ProjectManagement = () => {
-  const { activeId, setActiveId, projects, isLoading, isEmpty, hasData, deleteModal } =
-    useProjectsPageModel();
+  const {
+    activeId,
+    setActiveId,
+    projects,
+    filteredProjects,
+    isLoading,
+    isEmpty,
+    hasData,
+    deleteModal,
+  } = useProjectsPageModel();
 
   return (
     <PageWrapper header={<Header title="projects" />} sectionClassName={styles.projects}>
-      <Toolbar activeId={activeId} setActiveId={setActiveId} />
+      <Toolbar activeId={activeId} setActiveId={setActiveId} projects={projects} />
 
       {isEmpty && <EmptyState />}
 
       {(isLoading || hasData) && (
         <ProjectsTable
           isLoading={isLoading}
-          projects={projects}
+          projects={filteredProjects}
           onDelete={deleteModal.openDeleteModal}
         />
       )}

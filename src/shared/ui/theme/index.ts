963431ff2e60cabe6ed2ec996fd3cadd99dc0382
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-21 01:47:36 +0300

    Feat: update BREAKPOINTS to use numeric values and export Breakpoint type

--- src/shared/ui/theme/index.ts
+++ src/shared/ui/theme/index.ts
@@ -1,2 +1,2 @@
-export { BREAKPOINTS } from './lib/breakpoints';
+export { BREAKPOINTS, type Breakpoint } from './lib/breakpoints';
 export { OPTION_ICON_SHRINK, OPTION_STYLES } from './lib/styles';

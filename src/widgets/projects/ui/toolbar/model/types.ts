9f69c5ebabcf6e4dc3239b638b3fbb15748c1133
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-20 20:51:30 +0300

    Feat: refactor ToolbarProps and related types to include platformId and update structure

--- src/widgets/projects/ui/toolbar/model/types.ts
+++ src/widgets/projects/ui/toolbar/model/types.ts
@@ -1,34 +1,47 @@
-import type { ActiveTabProps, ProjectsHeaderProps, TabId } from '@types';
+import type { PlatformId, ProjectsHeaderProps, TabId } from '@types';
+
+import type { Dispatch, SetStateAction } from 'react';
 
 import type { Project } from '@/entities/project';
 
-export type ToolbarProps = ActiveTabProps & {
+export type ToolbarProps = {
+  statusId: TabId;
+  setStatusId: Dispatch<SetStateAction<TabId>>;
+  platformId: PlatformId | null;
+  setPlatformId: Dispatch<SetStateAction<PlatformId | null>>;
   projects: Project[];
 };
 
-export type ToolbarSelectProps = ActiveTabProps;
+export type ToolbarSelectProps = {
+  statusId: TabId;
+  setStatusId: Dispatch<SetStateAction<TabId>>;
+};
 
 export type ToolbarSelectOption = {
   value: TabId;
   label: string;
 };
 
-export type ToolbarHeaderProps = ActiveTabProps & {
+export type ToolbarHeaderProps = ToolbarSelectProps & {
   isSidebarOpen?: boolean;
 };
 
-export type ToolbarActionsProps = ActiveTabProps & {
+export type ToolbarActionsProps = {
   handleCreateProject?: () => void;
   projects: Project[];
+  platformId: PlatformId | null;
+  setPlatformId: Dispatch<SetStateAction<PlatformId | null>>;
 };
 
 export type ToolbarHeaderItemProps = ProjectsHeaderProps;
 
 export type PlatformOption = {
-  value: TabId;
+  value: PlatformId | null;
   label: string;
 };
 
-export type PlatformSelectProps = ActiveTabProps & {
+export type PlatformSelectProps = {
+  platformId: PlatformId | null;
+  setPlatformId: Dispatch<SetStateAction<PlatformId | null>>;
   projects: Project[];
 };

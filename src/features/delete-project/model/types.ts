3e87b662e7113ea3c9c050f9067c3d0f26409a8f
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-24 22:49:44 +0300

    Feat: reorder onClose method in DeleteModalActionsProps for consistency

--- src/features/delete-project/model/types.ts
+++ src/features/delete-project/model/types.ts
@@ -3,9 +3,9 @@ export type DeleteModalActionsProps = {
   canDelete: boolean;
   isDeleting: boolean;
   error: string | null;
-  onClose: () => void;
   onReasonChange: (reason: string) => void;
   onDelete: () => void;
+  onClose: () => void;
 };
 
 export type DeleteModalProps = DeleteModalActionsProps & {

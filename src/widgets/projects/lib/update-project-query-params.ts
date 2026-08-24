5628cdd13fe00e00be5cb1550807129757f4c71a
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-24 20:40:59 +0300

    Feat: add functions to get and update project query parameters

---
+++ src/widgets/projects/lib/update-project-query-params.ts
@@ -0,0 +1,45 @@
+import type { PlatformId, TabId } from '@types';
+
+import { PROJECT_QUERY_KEYS, PROJECT_TOOLBAR_NAMES } from './constants';
+
+const { SEARCH, STATUS, PLATFORM } = PROJECT_QUERY_KEYS;
+const { ALL_PROJECTS } = PROJECT_TOOLBAR_NAMES;
+
+export const updateProjectQueryParams = (
+  searchParams: URLSearchParams,
+  updates: Partial<{
+    search: string;
+    status: TabId;
+    platform: PlatformId | null;
+  }>,
+) => {
+  const params = new URLSearchParams(searchParams);
+
+  if ('search' in updates) {
+    const value = updates.search ?? '';
+
+    if (value.trim()) {
+      params.set(SEARCH, value);
+    } else {
+      params.delete(SEARCH);
+    }
+  }
+
+  if ('status' in updates) {
+    if (!updates.status || updates.status === ALL_PROJECTS) {
+      params.delete(STATUS);
+    } else {
+      params.set(STATUS, updates.status);
+    }
+  }
+
+  if ('platform' in updates) {
+    if (updates.platform == null) {
+      params.delete(PLATFORM);
+    } else {
+      params.set(PLATFORM, updates.platform);
+    }
+  }
+
+  return params;
+};

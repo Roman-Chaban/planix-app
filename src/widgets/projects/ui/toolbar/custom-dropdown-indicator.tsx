6520b55c8a984dcdb2da81006705f2564bc08578
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-24 20:06:51 +0300

    Feat: add custom dropdown indicator component for project toolbar

---
+++ src/widgets/projects/ui/toolbar/custom-dropdown-indicator.tsx
@@ -0,0 +1,8 @@
+import type { DropdownIndicatorProps } from 'react-select';
+
+import { CustomDropdownIndicator } from '@/shared/ui';
+import { PlatformDropdownIcon } from '@/shared/ui/icons';
+
+export const DropdownIndicator = <Option = unknown, IsMulti extends boolean = false>(
+  props: DropdownIndicatorProps<Option, IsMulti>,
+) => <CustomDropdownIndicator {...props} icon={<PlatformDropdownIcon />} />;

f21160425feddc1f398a719106039cb811b22ce1
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-24 20:41:18 +0300

    Feat: update setPlatformId type to a function signature for clarity

--- src/widgets/projects/model/types.ts
+++ src/widgets/projects/model/types.ts
@@ -1,9 +1,7 @@
 import type { PlatformId, Projects } from '@types';
 
-import type { Dispatch, SetStateAction } from 'react';
-
 export type UsePlatformSelectOptions = {
   platformId: PlatformId | null;
-  setPlatformId: Dispatch<SetStateAction<PlatformId | null>>;
+  setPlatformId: (platform: PlatformId | null) => void;
   projects: Projects;
 };

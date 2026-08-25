a593abe4de7b148401d9086f315167a412882477
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-25 22:25:26 +0300

    Feat: update hardcode literals with FILE_TYPES constant

--- src/widgets/project-details/ui/file-item/model/constants.ts
+++ src/widgets/project-details/ui/file-item/model/constants.ts
@@ -1,10 +1,14 @@
 import type { FileConfiguration } from './types';
 
+import { FILE_TYPES } from '@/shared/ui/file-upload';
+
 import { PdfIcon, UploadIcon } from '@/shared/ui/icons';
 
+const { PDF, IMAGE } = FILE_TYPES;
+
 export const FILE_CONFIGURATIONS: FileConfiguration = {
-  pdf: { icon: PdfIcon, type: 'pdf' },
-  png: { icon: UploadIcon, type: 'image' },
-  jpg: { icon: UploadIcon, type: 'image' },
-  jpeg: { icon: UploadIcon, type: 'image' },
-};
+  pdf: { icon: PdfIcon, type: PDF },
+  png: { icon: UploadIcon, type: IMAGE },
+  jpg: { icon: UploadIcon, type: IMAGE },
+  jpeg: { icon: UploadIcon, type: IMAGE },
+} as const;

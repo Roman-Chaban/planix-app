dcd79c0309c4dfe743c415af73e2d9a3d4cccf71
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-26 21:29:33 +0300

    Fix: change button preset to correct name

--- src/widgets/projects/ui/toolbar/toolbar-actions.tsx
+++ src/widgets/projects/ui/toolbar/toolbar-actions.tsx
@@ -52,7 +52,7 @@ export const ToolbarActions = ({
 
       <ProjectButton
         className={styles.addButton}
-        preset="ADD"
+        preset="CREATE_PROJECT"
         onClick={navigateToCreate}
         label={t('toolbar.addProjectButton')}
       />

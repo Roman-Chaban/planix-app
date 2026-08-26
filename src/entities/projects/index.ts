59f26ee68b99444cd76d2e2e71c7d9ceeafa6506
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-26 20:13:56 +0300

    Feat: add hooks export in index

--- src/entities/projects/index.ts
+++ src/entities/projects/index.ts
@@ -6,8 +6,10 @@ export type {
   ProjectFile,
 } from './model/types';
 
+export { useMyProjects } from './model/use-my-projects';
+export { useProjectActions } from './model/use-project-actions';
+export { useProjects } from './model/use-projects';
+
 export { getProjects, updateProject, createProject, deleteProject, uploadProjectFile } from './api';
 
 export { toProjectTableItem } from './lib/to-project-table-item';
-
-export { useProjectActions } from './model/use-project-actions';

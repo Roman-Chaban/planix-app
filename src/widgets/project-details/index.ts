3a63d73d43396d604c30a998ecd3df1ba950f4e0
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-26 21:28:09 +0300

    Feat: delete create translation keys for en, uk,  update index

--- src/widgets/project-details/index.ts
+++ src/widgets/project-details/index.ts
@@ -1,5 +1,4 @@
 export { Description } from './ui/description';
 export { FileItem } from './ui/file-item';
-export { ProjectHeader, getHeaderItems } from './ui/header';
 export { InfoCard, InfoList } from './ui/info';
 export { ProjectDetails } from './ui/project-details';

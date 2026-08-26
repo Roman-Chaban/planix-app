b3662f7940826835d63756448093f45258d930e3
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-26 23:55:01 +0300

    Feat: add MAIN_FILE_ACCEPT and PHOTO_FILE_ACCEPT constants

--- src/shared/ui/file-upload/model/constants.ts
+++ src/shared/ui/file-upload/model/constants.ts
@@ -2,3 +2,6 @@ export const FILE_TYPES = {
   PDF: 'pdf',
   IMAGE: 'image',
 } as const;
+
+export const MAIN_FILE_ACCEPT = '.pdf,.doc,.docx,.xls,.xlsx';
+export const PHOTO_FILE_ACCEPT = 'image/*';

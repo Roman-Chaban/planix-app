0ef0259c47e3bb2bfc4cd93928c6c7b38d37701a
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-27 23:21:03 +0300

    Fix: update import path in getClientAvatar

--- src/entities/projects/lib/get-client-avatar.ts
+++ src/entities/projects/lib/get-client-avatar.ts
@@ -1,6 +1,6 @@
 import type { ProjectFile } from '../model/types';
 
-import { AVATAR_EXTENSIONS } from '@/shared/ui/avatar/lib/constants';
+import { AVATAR_EXTENSIONS } from '@/shared/ui/avatar/model/constants';
 
 export const getClientAvatar = (files: ProjectFile[]): string => {
   return files.find((file) => AVATAR_EXTENSIONS.test(file.name))?.url ?? '';

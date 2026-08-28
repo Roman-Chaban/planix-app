e565c8ecdc35c9cfee95f84be89b8711655bd4ba
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-28 20:36:45 +0300

    Fix: move skeleton props in to the component, delete types file

--- src/shared/ui/skeleton/ui/skeleton.tsx
+++ src/shared/ui/skeleton/ui/skeleton.tsx
@@ -1,8 +1,12 @@
-import type { SkeletonProps } from '../model/types';
-
 import { Box } from '../../box';
 
 import styles from './skeleton.module.scss';
+
+export type SkeletonProps = {
+  width?: string | number;
+  height?: string | number;
+  radius?: string | number;
+};
 
 export const Skeleton = ({ width = '100%', height = 20, radius = '0' }: SkeletonProps) => {
   const geometry = { width, height, borderRadius: radius };

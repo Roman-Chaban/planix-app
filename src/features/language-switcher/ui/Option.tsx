f9aa806cac72b87b773f7d88eaba90e1d2d92047
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-28 20:24:28 +0300

    Feat: delete useless internal styles in SingleValue and Option components

--- src/features/language-switcher/ui/Option.tsx
+++ src/features/language-switcher/ui/Option.tsx
@@ -2,15 +2,14 @@ import { type OptionProps, components } from 'react-select';
 
 import type { LanguageOption } from '@/features/language-switcher';
 import { Box, Typography } from '@/shared/ui';
-import { OPTION_STYLES, OPTION_ICON_SHRINK } from '@/shared/ui/theme/lib/styles';
 
 export const Option = (selectProps: OptionProps<LanguageOption>) => {
   const Icon = selectProps.data.icon;
 
   return (
     <components.Option {...selectProps}>
-      <Box style={OPTION_STYLES}>
-        <Icon style={OPTION_ICON_SHRINK} />
+      <Box>
+        <Icon />
         <Typography as="span">{selectProps.data.label}</Typography>
       </Box>
     </components.Option>
--- src/features/language-switcher/ui/SingleValue.tsx
+++ src/features/language-switcher/ui/SingleValue.tsx
@@ -3,16 +3,13 @@ import { type SingleValueProps, components } from 'react-select';
 import type { LanguageOption } from '@/features/language-switcher';
 import { Box, Typography } from '@/shared/ui';
 import { LanguageIcon } from '@/shared/ui/icons';
-import { OPTION_LABEL, OPTION_STYLES } from '@/shared/ui/theme/lib/styles';
 
 export const SingleValue = (valueProps: SingleValueProps<LanguageOption>) => {
   return (
     <components.SingleValue {...valueProps}>
-      <Box style={OPTION_STYLES}>
+      <Box>
         <LanguageIcon />
-        <Typography as="span" style={OPTION_LABEL}>
-          {valueProps.data.label}
-        </Typography>
+        <Typography as="span">{valueProps.data.label}</Typography>
       </Box>
     </components.SingleValue>
   );

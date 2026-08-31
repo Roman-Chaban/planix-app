78e8a1bb3840f91e3b16665bee301c8aba670126
Roman Chaban <roman.chaban1001@gmail.com>
2026-09-01 00:07:09 +0300

    Feat: update profile feature index exports

--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -1,2 +1,5 @@
 export { ProfileUpdateForm } from './update';
 export { Notifications } from './notifications';
+export { ChangePasswordForm } from './change-password';
+export { LogoutContent, LogoutModal, LogoutModalGraphic } from './logout';
+export { SettingsSwitchItem, SettingsSwitchList } from './settings-switch';

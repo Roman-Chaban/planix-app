70e3c03aaab3e6cb9f0ed6a5522aac05afbad760
Roman Chaban <roman.chaban1001@gmail.com>
2026-08-31 23:58:44 +0300

    Feat: rename and correct the notifications feature name

--- src/features/profile/notifications/ui/Notifications.tsx
+++ src/features/profile/notifications/ui/Notifications.tsx
@@ -8,7 +8,7 @@ import { Box, SettingsHeader } from '@/shared/ui';
 import { SettingSwitchList } from '../../settings-switch/ui/SettingSwitchList';
 import { notificationsList } from '../lib/notifications-list';
 
-import styles from './Notifications.module.scss';
+import styles from './notifications.module.scss';
 
 export const Notifications = () => {
   const { t } = useTranslation(NS.SETTINGS);

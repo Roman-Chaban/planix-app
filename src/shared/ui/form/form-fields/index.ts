1b54a7d3010553597e3d93766412192acf62e5a2
Roman Chaban <roman.chaban1001@gmail.com>
2026-09-02 01:35:02 +0300

    Feat: expand exported types in form-fields index for enhanced field configurations

--- src/shared/ui/form/form-fields/index.ts
+++ src/shared/ui/form/form-fields/index.ts
@@ -1,3 +1,20 @@
-export type { FormFieldConfig, FormFieldKind, FormFieldsProps } from './model/types';
+export type {
+  FormFieldConfig,
+  FormFieldKind,
+  FormFieldsProps,
+  DateRangeFieldProps,
+  DefaultFieldConfig,
+  DefaultFieldKind,
+  DefaultFieldProps,
+  DefaultFieldValue,
+  FieldByKind,
+  FieldGroupLayout,
+  FileUploadFieldProps,
+  FormFieldFeature,
+  FormFieldLayout,
+  SharedFieldRendererProps,
+  TextareaFieldProps,
+} from './model/types';
 
 export { FormFields } from './ui/form-fields';
+export { DateRangeField, DefaultField, FileUploadField, TextareaField } from './ui/renderers';

aea4c16dce1bc69b73efd6981451b4f630b72938
Roman Chaban <roman.chaban1001@gmail.com>
2026-09-02 23:41:45 +0300

    Feat: add ClientCell and DueDateCell components for project table

--- src/widgets/projects/ui/projects-table/cells.tsx
+++ src/widgets/projects/ui/projects-table/cells.tsx
@@ -2,6 +2,8 @@ import type { TableItem } from './model/types';
 
 import { Avatar, Box } from '@/shared/ui';
 
+import { CalendarIcon } from '@/shared/ui/icons';
+
 import styles from './projects-table.module.scss';
 
 export const ClientCell = ({ client_name, client_avatar }: TableItem) => {
@@ -9,6 +11,15 @@ export const ClientCell = ({ client_name, client_avatar }: TableItem) => {
     <Box className={styles.clientCell}>
       <Avatar src={client_avatar} width={28} height={28} alt={client_name} />
       {client_name}
+    </Box>
+  );
+};
+
+export const DueDateCell = ({ due_date }: TableItem) => {
+  return (
+    <Box className={styles.dueDateCell}>
+      {<CalendarIcon />}
+      {due_date}
     </Box>
   );
 };
